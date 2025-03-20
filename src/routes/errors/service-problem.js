76ed84a85597457fc6cd4c89aed01c4f999b1743
Amirs Ishmuhametovs <amir.ishmuhametov@gmail.com>
2025-03-20 15:11:16 +0000

    changed desription in heading and title

--- src/routes/errors/service-problem.js
+++ src/routes/errors/service-problem.js
@@ -3,8 +3,8 @@ export const serviceProblem = {
   path: '/errors/service-problem',
   handler: (_request, h) => {
     return h.view('errors/service-problem', {
-      pageTitle: 'Service Problem',
-      heading: 'Sorry, there is a problem with the service'
+      pageTitle: 'Sorry, there is a problem with the service - Manage your land and farm businesses - GOV.UK',
+      heading: 'There is a problem with this service, but you can contact us.'
     })
   }
 }

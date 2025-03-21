a57088061bd021ab55feaeaacd1db5aa8ee9d50d
Amirs Ishmuhametovs <amir.ishmuhametov@gmail.com>
2025-03-21 13:04:26 +0000

    added unhappy path test coverage

--- test/unit/utils/errors.test.js
+++ test/unit/utils/errors.test.js
@@ -72,6 +72,14 @@ describe('#catchAll', () => {
       StatusCodes.SERVICE_UNAVAILABLE
     )
   })
+  test('Should NOT render service-unavailable page for INTERNAL_SERVER_ERROR', () => {
+    catchAll(mockRequest(StatusCodes.INTERNAL_SERVER_ERROR), mockToolkit)
+    expect(mockErrorLogger).toHaveBeenCalledWith(mockStack)
+    expect(mockToolkitView).not.toHaveBeenCalledWith('errors/service-unavailable', expect.anything())
+    expect(mockToolkitCode).toHaveBeenCalledWith(
+      StatusCodes.INTERNAL_SERVER_ERROR
+    )
+  })
 
   test('Should provide expected "Forbidden" page', () => {
     catchAll(mockRequest(StatusCodes.FORBIDDEN), mockToolkit)

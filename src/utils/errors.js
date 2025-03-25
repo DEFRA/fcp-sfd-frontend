30e51b5c069dbc116077cf5777a592f03e4b1e4d
Amirs Ishmuhametovs <amir.ishmuhametov@gmail.com>
2025-03-25 15:06:40 +0000

    resolved comment, added default error view until we get bespoke

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,27 +1,11 @@
 import { StatusCodes } from 'http-status-codes'
 
-function statusCodeMessage (statusCode) {
-  switch (statusCode) {
-    case StatusCodes.NOT_FOUND:
-      return 'Page not found'
-    case StatusCodes.FORBIDDEN:
-      return 'Forbidden'
-    case StatusCodes.UNAUTHORIZED:
-      return 'Unauthorized'
-    case StatusCodes.BAD_REQUEST:
-      return 'Bad Request'
-    default:
-      return 'Something went wrong'
-  }
-}
-
 export function catchAll (request, h) {
   if (!request.response || !('isBoom' in request.response)) {
     return h.continue
   }
 
   const statusCode = request.response.output.statusCode
-  const errorMessage = statusCodeMessage(statusCode)
 
   switch (statusCode) {
     case StatusCodes.SERVICE_UNAVAILABLE:
@@ -38,11 +22,7 @@ export function catchAll (request, h) {
 
     default:
       return h
-        .view('error', {
-          pageTitle: errorMessage,
-          heading: statusCode,
-          message: errorMessage
-        })
+        .view('errors/service-problem')
         .code(statusCode)
   }
 }

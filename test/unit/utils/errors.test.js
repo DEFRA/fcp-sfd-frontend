0adefb43c92f75d2c068865a242595c53ba877e3
Amirs Ishmuhametovs <amir.ishmuhametov@gmail.com>
2025-03-25 15:45:03 +0000

    fixed unit tests

--- test/unit/utils/errors.test.js
+++ test/unit/utils/errors.test.js
@@ -1,37 +1,11 @@
-import { createServer } from '../../../src/server.js'
 import { StatusCodes } from 'http-status-codes'
 import { catchAll } from '../../../src/utils/errors.js'
-import { jest, beforeAll, describe, test, expect, afterAll } from '@jest/globals'
-
-describe('#errors', () => {
-  let server
-
-  beforeAll(async () => {
-    server = await createServer()
-    await server.initialize()
-  })
-
-  afterAll(async () => {
-    await server.stop({ timeout: 0 })
-  })
-
-  test('Should provide expected Not Found page', async () => {
-    const { result, statusCode } = await server.inject({
-      method: 'GET',
-      url: '/non-existent-path'
-    })
-
-    expect(result).toEqual(
-      expect.stringContaining('Page not found | Single Front Door')
-    )
-    expect(statusCode).toBe(StatusCodes.NOT_FOUND)
-  })
-})
+import { jest, describe, test, expect } from '@jest/globals'
+// import { createServer } from '../../../src/server.js'
 
 describe('#catchAll', () => {
   const mockErrorLogger = jest.fn()
   const mockStack = 'Mock error stack'
-  const errorPage = 'error'
   const mockRequest = (statusCode) => ({
     response: {
       isBoom: true,
@@ -48,20 +22,6 @@ describe('#catchAll', () => {
     view: mockToolkitView.mockReturnThis(),
     code: mockToolkitCode.mockReturnThis()
   }
-
-  test('Should provide expected "Not Found" page', () => {
-    catchAll(mockRequest(StatusCodes.NOT_FOUND), mockToolkit)
-
-    expect(mockErrorLogger).not.toHaveBeenCalledWith(mockStack)
-    expect(mockToolkitView).toHaveBeenCalledWith(errorPage, {
-      pageTitle: 'Page not found',
-      heading: StatusCodes.NOT_FOUND,
-      message: 'Page not found'
-    })
-    expect(mockToolkitCode).toHaveBeenCalledWith(
-      StatusCodes.NOT_FOUND
-    )
-  })
 
   test('Should provide expected "Service-unavailable" page', () => {
     catchAll(mockRequest(StatusCodes.SERVICE_UNAVAILABLE), mockToolkit)
@@ -81,62 +41,6 @@ describe('#catchAll', () => {
     )
   })
 
-  test('Should provide expected "Forbidden" page', () => {
-    catchAll(mockRequest(StatusCodes.FORBIDDEN), mockToolkit)
-
-    expect(mockErrorLogger).not.toHaveBeenCalledWith(mockStack)
-    expect(mockToolkitView).toHaveBeenCalledWith(errorPage, {
-      pageTitle: 'Forbidden',
-      heading: StatusCodes.FORBIDDEN,
-      message: 'Forbidden'
-    })
-    expect(mockToolkitCode).toHaveBeenCalledWith(
-      StatusCodes.FORBIDDEN
-    )
-  })
-
-  test('Should provide expected "Unauthorized" page', () => {
-    catchAll(mockRequest(StatusCodes.UNAUTHORIZED), mockToolkit)
-
-    expect(mockErrorLogger).not.toHaveBeenCalledWith(mockStack)
-    expect(mockToolkitView).toHaveBeenCalledWith(errorPage, {
-      pageTitle: 'Unauthorized',
-      heading: StatusCodes.UNAUTHORIZED,
-      message: 'Unauthorized'
-    })
-    expect(mockToolkitCode).toHaveBeenCalledWith(
-      StatusCodes.UNAUTHORIZED
-    )
-  })
-
-  test('Should provide expected "Bad Request" page', () => {
-    catchAll(mockRequest(StatusCodes.BAD_REQUEST), mockToolkit)
-
-    expect(mockErrorLogger).not.toHaveBeenCalledWith(mockStack)
-    expect(mockToolkitView).toHaveBeenCalledWith(errorPage, {
-      pageTitle: 'Bad Request',
-      heading: StatusCodes.BAD_REQUEST,
-      message: 'Bad Request'
-    })
-    expect(mockToolkitCode).toHaveBeenCalledWith(
-      StatusCodes.BAD_REQUEST
-    )
-  })
-
-  test('Should provide expected default page', () => {
-    catchAll(mockRequest(StatusCodes.IM_A_TEAPOT), mockToolkit)
-
-    expect(mockErrorLogger).not.toHaveBeenCalledWith(mockStack)
-    expect(mockToolkitView).toHaveBeenCalledWith(errorPage, {
-      pageTitle: 'Something went wrong',
-      heading: StatusCodes.IM_A_TEAPOT,
-      message: 'Something went wrong'
-    })
-    expect(mockToolkitCode).toHaveBeenCalledWith(
-      StatusCodes.IM_A_TEAPOT
-    )
-  })
-
   test('Should provide expected service problem page and log error for internalServerError', () => {
     catchAll(
       mockRequest(StatusCodes.INTERNAL_SERVER_ERROR),
@@ -146,6 +50,14 @@ describe('#catchAll', () => {
     expect(mockToolkitView).toHaveBeenCalledWith('errors/service-problem')
     expect(mockToolkitCode).toHaveBeenCalledWith(
       StatusCodes.INTERNAL_SERVER_ERROR
+    )
+  })
+
+  test('Should provide service problem page for other status codes (fallback)', () => {
+    catchAll(mockRequest(StatusCodes.BAD_REQUEST), mockToolkit)
+    expect(mockToolkitView).toHaveBeenCalledWith('errors/service-problem')
+    expect(mockToolkitCode).toHaveBeenCalledWith(
+      StatusCodes.BAD_REQUEST
     )
   })
 })

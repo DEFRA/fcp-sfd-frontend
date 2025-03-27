bb96d3df814b11a39b9d44344755195f2e39f024
Amirs Ishmuhametovs <amir.ishmuhametov@gmail.com>
2025-03-27 09:03:16 +0000

    added meta name description to head element and adjusted the title of the service

--- src/routes/errors/service-problem.js
+++ src/routes/errors/service-problem.js
@@ -3,7 +3,7 @@ export const serviceProblem = {
   path: '/service-problem',
   handler: (_request, h) => {
     return h.view('errors/service-problem', {
-      pageTitle: 'Sorry, there is a problem with the service - Manage your land and farm businesses - GOV.UK',
+      pageTitle: 'Sorry, there is a problem with the service',
       heading: 'There is a problem with this service, but you can contact us.'
     })
   }

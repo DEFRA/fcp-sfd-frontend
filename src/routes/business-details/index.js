c7d2abf5e12079a72e004e291d872fde57e20179
Amirs Ismuhametovs <98330195+JungaBunga@users.noreply.github.com>
2025-04-14 16:11:57 +0100

    Business check view (#19)

--- src/routes/business-details/index.js
+++ src/routes/business-details/index.js
@@ -2,10 +2,12 @@ import { businessNameChangeRoutes } from './business-name-change.js'
 import { businessNameCheckRoutes } from './business-name-check.js'
 import { businessDetailsRoutesView } from './business-details.js'
 import { businessAddressRoutes } from './business-address-form.js'
+import { businessAddressCheckRoutes } from './business-address-check.js'
 
 export const businessDetailsRoutes = [
   ...businessNameChangeRoutes,
   ...businessNameCheckRoutes,
   ...businessDetailsRoutesView,
-  ...businessAddressRoutes
+  ...businessAddressRoutes,
+  ...businessAddressCheckRoutes
 ]

b3e4d7f4826edb35ead21d859b624cac523b06a0
Rana Salem <ranatasalem@gmail.com>
2025-04-22 22:53:08 +0100

    update errors/validation

--- src/schemas/business-details/business-phone.js
+++ src/schemas/business-details/business-phone.js
@@ -1,28 +1,28 @@
 import Joi from 'joi'
-import { 
-  BUSINESS_PHONE_NUMBER_MIN, 
-  BUSINESS_PHONE_NUMBER_MAX 
+import {
+  BUSINESS_PHONE_NUMBER_MIN,
+  BUSINESS_PHONE_NUMBER_MAX
 } from '../../constants/validation-fields.js'
 
 export const businessPhoneSchema = Joi.object({
   businessTelephone: Joi.string()
+    .empty('')
     .min(BUSINESS_PHONE_NUMBER_MIN)
     .max(BUSINESS_PHONE_NUMBER_MAX)
     .messages({
-      'string.empty': 'Enter business phone number',
-      'string.max': `Business phone number must be between ${BUSINESS_PHONE_NUMBER_MIN} and ${BUSINESS_PHONE_NUMBER_MAX}`,
-      'any.required': 'Enter business phone number'
+      'string.min': `Business phone number must be between ${BUSINESS_PHONE_NUMBER_MIN} and ${BUSINESS_PHONE_NUMBER_MAX} characters`,
+      'string.max': `Business phone number must be between ${BUSINESS_PHONE_NUMBER_MIN} and ${BUSINESS_PHONE_NUMBER_MAX} characters`
     }),
   businessMobile: Joi.string()
+    .empty('')
     .min(BUSINESS_PHONE_NUMBER_MIN)
     .max(BUSINESS_PHONE_NUMBER_MAX)
     .messages({
-      'string.empty': 'Enter business phone number',
-      'string.max': `Business phone number must be between ${BUSINESS_PHONE_NUMBER_MIN} and ${BUSINESS_PHONE_NUMBER_MAX}`,
-      'any.required': 'Enter business phone number'
+      'string.min': `Business phone number must be between ${BUSINESS_PHONE_NUMBER_MIN} and ${BUSINESS_PHONE_NUMBER_MAX} characters`,
+      'string.max': `Business phone number must be between ${BUSINESS_PHONE_NUMBER_MIN} and ${BUSINESS_PHONE_NUMBER_MAX} characters`
     })
 })
-.or('businessTelephone', 'businessMobile')
-.messages({
-  'object.missing': 'Enter at least one business phone number'
-})
+  .or('businessTelephone', 'businessMobile')
+  .messages({
+    'object.missing': 'Enter at least one business phone number'
+  })

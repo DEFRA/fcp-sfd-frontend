efa05192b0544084296fa5fa856e35b17b7ab1e0
Rana Salem <ranatasalem@gmail.com>
2025-04-23 09:35:29 +0100

    update error messages

--- src/schemas/business-details/business-phone.js
+++ src/schemas/business-details/business-phone.js
@@ -10,16 +10,16 @@ export const businessPhoneSchema = Joi.object({
     .min(BUSINESS_PHONE_NUMBER_MIN)
     .max(BUSINESS_PHONE_NUMBER_MAX)
     .messages({
-      'string.min': `Business telephone number must be between ${BUSINESS_PHONE_NUMBER_MIN} and ${BUSINESS_PHONE_NUMBER_MAX} characters`,
-      'string.max': `Business telephone number must be between ${BUSINESS_PHONE_NUMBER_MIN} and ${BUSINESS_PHONE_NUMBER_MAX} characters`
+      'string.min': `Business telephone number must be ${BUSINESS_PHONE_NUMBER_MIN} characters or more`,
+      'string.max': `Business telephone number must be ${BUSINESS_PHONE_NUMBER_MAX} characters or less`
     }),
   businessMobile: Joi.string()
     .empty('')
     .min(BUSINESS_PHONE_NUMBER_MIN)
     .max(BUSINESS_PHONE_NUMBER_MAX)
     .messages({
-      'string.min': `Business mobile number must be between ${BUSINESS_PHONE_NUMBER_MIN} and ${BUSINESS_PHONE_NUMBER_MAX} characters`,
-      'string.max': `Business mobile number must be between ${BUSINESS_PHONE_NUMBER_MIN} and ${BUSINESS_PHONE_NUMBER_MAX} characters`
+      'string.min': `Business mobile phone number must be ${BUSINESS_PHONE_NUMBER_MIN} characters or more`,
+      'string.max': `Business mobile phone number must be ${BUSINESS_PHONE_NUMBER_MAX} characters or less`
     })
 })
   .or('businessTelephone', 'businessMobile')

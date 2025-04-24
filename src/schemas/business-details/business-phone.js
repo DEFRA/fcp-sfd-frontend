9504e0f5802ed184ce5b5760c62e4cc9d8c544ff
Rana Salem <ranatasalem@gmail.com>
2025-04-24 15:07:12 +0100

    update phone number constants

--- src/schemas/business-details/business-phone.js
+++ src/schemas/business-details/business-phone.js
@@ -1,25 +1,25 @@
 import Joi from 'joi'
 import {
-  BUSINESS_PHONE_NUMBER_MIN,
-  BUSINESS_PHONE_NUMBER_MAX
+  PHONE_NUMBER_MIN,
+  PHONE_NUMBER_MAX
 } from '../../constants/validation-fields.js'
 
 export const businessPhoneSchema = Joi.object({
   businessTelephone: Joi.string()
     .empty('')
-    .min(BUSINESS_PHONE_NUMBER_MIN)
-    .max(BUSINESS_PHONE_NUMBER_MAX)
+    .min(PHONE_NUMBER_MIN)
+    .max(PHONE_NUMBER_MAX)
     .messages({
-      'string.min': `Business telephone number must be ${BUSINESS_PHONE_NUMBER_MIN} characters or more`,
-      'string.max': `Business telephone number must be ${BUSINESS_PHONE_NUMBER_MAX} characters or less`
+      'string.min': `Business telephone number must be ${PHONE_NUMBER_MIN} characters or more`,
+      'string.max': `Business telephone number must be ${PHONE_NUMBER_MAX} characters or less`
     }),
   businessMobile: Joi.string()
     .empty('')
-    .min(BUSINESS_PHONE_NUMBER_MIN)
-    .max(BUSINESS_PHONE_NUMBER_MAX)
+    .min(PHONE_NUMBER_MIN)
+    .max(PHONE_NUMBER_MAX)
     .messages({
-      'string.min': `Business mobile phone number must be ${BUSINESS_PHONE_NUMBER_MIN} characters or more`,
-      'string.max': `Business mobile phone number must be ${BUSINESS_PHONE_NUMBER_MAX} characters or less`
+      'string.min': `Business mobile phone number must be ${PHONE_NUMBER_MIN} characters or more`,
+      'string.max': `Business mobile phone number must be ${PHONE_NUMBER_MAX} characters or less`
     })
 })
   .or('businessTelephone', 'businessMobile')

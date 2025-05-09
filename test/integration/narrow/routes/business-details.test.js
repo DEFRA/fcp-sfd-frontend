4c04bad834ef07a88cb7c55380cda9f71d380fef
Rana Salem <ranatasalem@gmail.com>
2025-05-09 10:20:54 +0100

    complete refactoring of all integration tests for GET routes

--- test/integration/narrow/routes/business-details.test.js
+++ test/integration/narrow/routes/business-details.test.js
@@ -15,52 +15,54 @@ describe('business details', () => {
     return server
   }
 
-  describe('With Error Views Enabled', () => {
-    let server
+  let server
 
+  beforeEach(async () => {
+    server = await resetAndCreateServer()
+  })
+
+  afterEach(async () => {
+    await server.stop()
+  })
+
+  describe('with error views enabled', () => {
     beforeAll(() => {
       process.env.ALLOW_ERROR_VIEWS = 'true'
     })
 
-    beforeEach(async () => {
-      server = await resetAndCreateServer()
-    })
+    describe('GET routes', () => {
+      test('health route responds correctly', async () => {
+        const response = await server.inject({
+          method: 'GET',
+          url: '/health'
+        })
 
-    afterEach(async () => {
-      await server.stop()
-    })
-
-    test('health route responds correctly', async () => {
-      const response = await server.inject({
-        method: 'GET',
-        url: '/health'
+        expect(response.statusCode).toBe(200)
       })
 
-      expect(response.statusCode).toBe(200)
-    })
-    
-    test.each([
-      ['home', '/'],
-      ['cookies', '/cookies'],
-      ['service unavailable', '/service-unavailable'],
-      ['page not found', '/page-not-found'],
-      ['service problem', '/service-problem'],
-      ['business details', '/business-details'],
-      ['change business name (GET)', '/business-name-change'],
-      ['check business name', '/business-name-check'],
-      ['enter business address (GET)', '/business-address-enter'],
-      ['check business address', '/business-address-check'],
-      ['change business phone numbers (GET)', '/business-phone-numbers-change'],
-      ['check business phone numbers', '/business-phone-numbers-check'],
-      ['change business email (GET)', '/business-email-change'],
-      ['check business email', '/business-email-check'],
-      ['change business legal status', '/business-legal-status-change'],
-      ['change business type', '/business-type-change']
-    ])('%s route responds correctly', async (_, url) => {
-      const response = await server.inject({ method: 'GET', url })
+      test.each([
+        ['home', '/'],
+        ['cookies', '/cookies'],
+        ['service unavailable', '/service-unavailable'],
+        ['page not found', '/page-not-found'],
+        ['service problem', '/service-problem'],
+        ['business details', '/business-details'],
+        ['change business name', '/business-name-change'],
+        ['check business name', '/business-name-check'],
+        ['enter business address', '/business-address-enter'],
+        ['check business address', '/business-address-check'],
+        ['change business phone numbers', '/business-phone-numbers-change'],
+        ['check business phone numbers', '/business-phone-numbers-check'],
+        ['change business email', '/business-email-change'],
+        ['check business email', '/business-email-check'],
+        ['change business legal status', '/business-legal-status-change'],
+        ['change business type', '/business-type-change']
+      ])('%s route responds correctly', async (_, url) => {
+        const response = await server.inject({ method: 'GET', url })
 
-      expect(response.statusCode).toBe(200)
-      expect(response.headers['content-type']).toContain('text/html')
+        expect(response.statusCode).toBe(200)
+        expect(response.headers['content-type']).toContain('text/html')
+      })
     })
 
     test('business-name-change POST route is registered', async () => {
@@ -144,19 +146,9 @@ describe('business details', () => {
     })
   })
 
-  describe('With Error Views Disabled', () => {
-    let server
-
+  describe('with error views disabled', () => {
     beforeAll(() => {
       process.env.ALLOW_ERROR_VIEWS = 'false'
-    })
-
-    beforeEach(async () => {
-      server = await resetAndCreateServer()
-    })
-
-    afterEach(async () => {
-      await server.stop()
     })
 
     afterAll(() => {
@@ -167,51 +159,24 @@ describe('business details', () => {
       }
     })
 
-    test('home route still responds correctly', async () => {
-      const response = await server.inject({
-        method: 'GET',
-        url: '/'
-      })
+    test.each([
+      ['home', '/'],
+      ['service problem', '/contact-help']
+    ])('%s route responds correctly', async (_, url) => {
+      const response = await server.inject({ method: 'GET', url })
 
       expect(response.statusCode).toBe(200)
       expect(response.headers['content-type']).toContain('text/html')
     })
 
-    test('service-unavailable route returns 404 when disabled', async () => {
-      const response = await server.inject({
-        method: 'GET',
-        url: '/service-unavailable'
-      })
+    test.each([
+      ['page not found', '/page-not-found'],
+      ['service unavailable', '/service-unavailable'],
+      ['service problem', '/service-problem']
+    ])('%s route returns 404 when disabled', async (_, url) => {
+      const response = await server.inject({ method: 'GET', url })
 
       expect(response.statusCode).toBe(404)
-    })
-
-    test('page-not-found route returns 404 when disabled', async () => {
-      const response = await server.inject({
-        method: 'GET',
-        url: '/page-not-found'
-      })
-
-      expect(response.statusCode).toBe(404)
-    })
-
-    test('service-problem route returns 404 when disabled', async () => {
-      const response = await server.inject({
-        method: 'GET',
-        url: '/service-problem'
-      })
-
-      expect(response.statusCode).toBe(404)
-    })
-
-    test('service-problem route responds correctly', async () => {
-      const response = await server.inject({
-        method: 'GET',
-        url: '/contact-help'
-      })
-
-      expect(response.statusCode).toBe(200)
-      expect(response.headers['content-type']).toContain('text/html')
     })
   })
 })

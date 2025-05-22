2ef42bf055e2ea4bfdede77fa4bb8148b82db77e
Rebecca Ransome <beckyrose200@aol.com>
2025-05-22 12:11:11 +0100

    WIP

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,8 @@ import { home } from './home.js'
 import { staticAssetRoutes } from './static-assets.js'
 import { cookies } from './cookies.js'
 import { footerRoutes } from './footer/index.js'
-import { businessDetailsRoutes } from './business-details/index.js'
+// import { businessDetailsRoutes } from './business-details/index.js'
+import { businessDetailsRoutes } from './business-details.routes.js'
 
 export const routes = [
   health,

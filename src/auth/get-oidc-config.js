22eee5f107c97be27dc8668b15388e74527dd395
Abidemi Adio <abidemi.adio@defra.gov.uk>
2025-05-28 08:37:26 +0100

    Update IRO PR FB

--- src/auth/get-oidc-config.js
+++ src/auth/get-oidc-config.js
@@ -2,8 +2,6 @@ import Wreck from '@hapi/wreck'
 import config from '../config/index.js'
 
 async function getOidcConfig () {
-  // Fetch the OpenID Connect configuration from the well-known endpoint
-  // Contains the URLs for authorisation, sign out, token and public keys in JSON format
   const { payload } = await Wreck.get(config.get('defraId.wellKnownUrl'), {
     json: true
   })

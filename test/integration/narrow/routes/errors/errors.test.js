a3d8c772b89ebdc8228df9da642a768e749b5562
AbidemiAdio <98023713+AbidemiAdio@users.noreply.github.com>
2025-05-28 08:52:27 +0100

    DEFRA ID Initial fcp-sfd-frontend repository setup for DefraId implem… (#40)
    * DEFRA ID Initial fcp-sfd-frontend repository setup for DefraId implementation
    
    * Updated version number
    
    * Update IRO PR FB

--- test/integration/narrow/routes/errors/errors.test.js
+++ test/integration/narrow/routes/errors/errors.test.js
@@ -6,6 +6,8 @@ describe('Error Routes Registration', () => {
   const ERRORS_MODULE_PATH = '../../../../../src/routes/errors/index.js'
 
   describe('With Error Views Enabled', () => {
+    const hookTimeout = 50000
+
     let server
     let errors
 
@@ -22,11 +24,11 @@ describe('Error Routes Registration', () => {
       const { createServer } = await import(SERVER_MODULE_PATH)
       server = await createServer()
       await server.initialize()
-    })
+    }, hookTimeout)
 
     afterEach(async () => {
       await server.stop()
-    })
+    }, hookTimeout)
 
     test('service-unavailable route is included in errors array', () => {
       const serviceUnavailableRoute = errors.find(route =>

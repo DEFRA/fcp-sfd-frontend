49da33b766e3d8e23e76b64d3155ac021ca7d318
Rana Salem <ranatasalem@gmail.com>
2025-06-02 11:39:48 +0100

    update dal env var names

--- src/config/dal.js
+++ src/config/dal.js
@@ -4,13 +4,13 @@ export const dalConfig = {
       doc: 'Test email to communicate with data access layer (DAL)',
       format: 'String',
       default: 'test.user11@defra.gov.uk',
-      env: 'LOCAL_DAL_EMAIL_ADDRESS'
+      env: 'DAL_EMAIL_ADDRESS'
     },
     localDalEndpoint: {
       doc: 'Local endpoint to retrieve data from the data access layer (DAL)',
       format: 'String',
       default: 'http://fcp-dal-api:3005/graphql',
-      env: 'LOCAL_DAL_ENDPOINT'
+      env: 'DAL_ENDPOINT'
     }
   }
 }

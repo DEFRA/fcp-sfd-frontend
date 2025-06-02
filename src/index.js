f18481c482b73d89591c6a585a6677248244db1c
Rana Salem <ranatasalem@gmail.com>
2025-06-02 15:44:09 +0100

    temp log to show retrieval from dal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import process from 'node:process'
 import { createLogger } from './utils/logger.js'
 import { startServer } from './utils/start-server.js'
+import { dalConnecitonHandler } from './dal/connection-handler.js'
 
 await startServer()
+
+const query = `
+  query Business {
+  business(sbi: 107591843) {
+    sbi
+  }
+}
+`
+
+const dal = await dalConnecitonHandler(query)
+
+const response = dal.data
+
+console.log('------->', response)
 
 process.on('unhandledRejection', (error) => {
   const logger = createLogger()

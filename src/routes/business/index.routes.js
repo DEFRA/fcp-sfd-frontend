270bf94dbb7dd27583975d7498701b79c284e4a5
Rebecca Ransome <beckyrose200@aol.com>
2025-06-02 11:41:41 +0100

    Update: Alphabetise route ordering

--- src/routes/business/index.routes.js
+++ src/routes/business/index.routes.js
@@ -1,25 +1,25 @@
-import { businessNameChangeRoutes } from './business-name-change.routes.js'
-import { businessAddressCheckRoutes } from './business-address-check.routes.js/index.js'
-import { businessNameCheckRoutes } from './business-name-check.routes.js'
-import { businessDetailsRoutes } from './details.routes.js'
+import { businessAddressCheckRoutes } from './business-address-check.routes.js'
 import { businessAddressRoutes } from './business-address-enter.routes.js'
-import { businessPhoneNumbersChangeRoutes } from './business-phone-numbers-change.routes.js'
-import { businessPhoneNumbersCheckRoutes } from './business-phone-numbers-check.routes.js'
+import { businessDetailsRoutes } from './details.routes.js'
 import { businessEmailChangeRoutes } from './business-email-change.routes.js'
 import { businessEmailCheckRoutes } from './business-email-check.routes.js'
 import { businessLegalStatusRoutes } from './business-legal-status-change.routes.js'
+import { businessNameChangeRoutes } from './business-name-change.routes.js'
+import { businessNameCheckRoutes } from './business-name-check.routes.js'
+import { businessPhoneNumbersChangeRoutes } from './business-phone-numbers-change.routes.js'
+import { businessPhoneNumbersCheckRoutes } from './business-phone-numbers-check.routes.js'
 import { businessTypeRoutes } from './business-type-change.routes.js'
 
 export const businessRoutes = [
-  ...businessDetailsRoutes,
-  ...businessNameChangeRoutes,
-  ...businessNameCheckRoutes,
-  ...businessAddressRoutes,
   ...businessAddressCheckRoutes,
-  ...businessPhoneNumbersChangeRoutes,
-  ...businessPhoneNumbersCheckRoutes,
+  ...businessAddressRoutes,
+  ...businessDetailsRoutes,
   ...businessEmailChangeRoutes,
   ...businessEmailCheckRoutes,
   ...businessLegalStatusRoutes,
+  ...businessNameChangeRoutes,
+  ...businessNameCheckRoutes,
+  ...businessPhoneNumbersChangeRoutes,
+  ...businessPhoneNumbersCheckRoutes,
   ...businessTypeRoutes
 ]

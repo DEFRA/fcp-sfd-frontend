d984ff2f8a8213434a78291872382bb078905a3f
Rebecca Ransome <beckyrose200@aol.com>
2025-06-03 14:19:32 +0100

    Add: Presenter

---
+++ src/presenters/business/business-details.presenter.js
@@ -0,0 +1,45 @@
+/**
+ * Formats data ready for presenting in the `/business-details` page
+ * @module businessDetailsPresenter
+ */
+
+/**
+ * Formats the data ready for presenting in the `/business-details` page
+ *
+ * @param {object} data - The business details data fetched from the API
+ * @param {object} yar - The Hapi `request.yar` session manager
+ *
+ * @returns {object} page data needed for the `/business-details` page
+ */
+const businessDetailsPresenter = (data) => {
+  // Need to sort out the banner
+  return {
+    pageTitle: 'View and update your business details',
+    metaDescription: 'View and change the details for your business.',
+    address: _formatAddress(data.businessAddress),
+    businessName: data.businessName,
+    businessTelephone: data.businessTelephone,
+    businessMobile: data.businessMobile,
+    businessEmail: data.businessEmail,
+    singleBusinessIdentifier: data.singleBusinessIdentifier ?? null,
+    vatNumber: data.vatNumber ?? null,
+    tradeNumber: data.tradeNumber ?? null,
+    vendorRegistrationNumber: data.vendorRegistrationNumber ?? null,
+    countyParishHoldingNumber: data.countyParishHoldingNumber ?? null,
+    businessLegalStatus: data.businessLegalStatus ?? null,
+    businessType: data.businessType ?? null,
+    userName: data.userName
+  }
+}
+
+/**
+ * Formats the business address by removing any falsy values (e.g. empty strings, null, undefined)
+ * @private
+ */
+const _formatAddress = (businessAddress) => {
+  return Object.values(businessAddress).filter(Boolean)
+}
+
+export {
+  businessDetailsPresenter
+}

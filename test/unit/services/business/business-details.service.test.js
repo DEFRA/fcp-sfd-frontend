688086a15503279bafee2e8626d6602d64c60194
Rebecca Ransome <beckyrose200@aol.com>
2025-06-03 14:20:29 +0100

    Add: Business details service

---
+++ test/unit/services/business/business-details.service.test.js
@@ -0,0 +1,76 @@
+// Test framework dependencies
+import { describe, test, expect, beforeEach, vi } from 'vitest'
+
+// Things we need to mock
+import { fetchBusinessDetailsService } from '../../../../src/services/business/fetch-business-details.service.js'
+
+// Thing under test
+import { businessDetailsService } from '../../../../src/services/business/business-details.service.js'
+
+// Mock
+vi.mock('../../../../src/services/business/fetch-business-details.service.js', () => ({
+  fetchBusinessDetailsService: vi.fn()
+}))
+
+describe('businessDetailsService', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  describe('when called', () => {
+    beforeEach(() => {
+      const mockData = {
+        businessName: 'Agile Farm Ltd',
+        businessAddress: {
+          address1: '10 Skirbeck Way',
+          address2: '',
+          city: 'Maidstone',
+          county: '',
+          postcode: 'SK22 1DL',
+          country: 'United Kingdom'
+        },
+        businessTelephone: '01234567890',
+        businessMobile: '01234567890',
+        businessEmail: 'a.farmer@farms.com',
+        singleBusinessIdentifier: '123456789',
+        vatNumber: '',
+        tradeNumber: '987654',
+        vendorRegistrationNumber: '699368',
+        countyParishHoldingNumber: '12/563/0998',
+        businessLegalStatus: 'Sole proprietorship',
+        businessType: 'Central or local government',
+        userName: 'Alfred Waldron'
+      }
+
+      fetchBusinessDetailsService.mockResolvedValue(mockData)
+    })
+
+    test('it fetches the business details data', async () => {
+      await businessDetailsService()
+
+      expect(fetchBusinessDetailsService).toHaveBeenCalled()
+    })
+
+    test('returns the page data for the view', async () => {
+      const result = await businessDetailsService()
+
+      expect(result).toEqual({
+        pageTitle: 'View and update your business details',
+        metaDescription: 'View and change the details for your business.',
+        address: ['10 Skirbeck Way', 'Maidstone', 'SK22 1DL', 'United Kingdom'],
+        businessName: 'Agile Farm Ltd',
+        businessTelephone: '01234567890',
+        businessMobile: '01234567890',
+        businessEmail: 'a.farmer@farms.com',
+        singleBusinessIdentifier: '123456789',
+        vatNumber: '',
+        tradeNumber: '987654',
+        vendorRegistrationNumber: '699368',
+        countyParishHoldingNumber: '12/563/0998',
+        businessLegalStatus: 'Sole proprietorship',
+        businessType: 'Central or local government',
+        userName: 'Alfred Waldron'
+      })
+    })
+  })
+})

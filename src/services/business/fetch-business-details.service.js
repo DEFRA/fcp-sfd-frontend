7007e3c2eea9ef43a236b5d854a616d8f2e6da7a
Rebecca Ransome <beckyrose200@aol.com>
2025-06-03 14:25:54 +0100

    Add: Fetch business details service

---
+++ src/services/business/fetch-business-details.service.js
@@ -0,0 +1,36 @@
+/**
+ * Fetches the business details associated with the logged in users business
+ * @module fetchBusinessDetailsService
+ */
+
+const fetchBusinessDetailsService = async (_request) => {
+  // TODO: Remove stubbed data and instead call the API to get the business details associated with the users log in
+  // This will be using the consolidated view API
+  // The data needed for the business details page
+  return {
+    businessName: 'Agile Farm Ltd',
+    businessAddress: {
+      address1: '10 Skirbeck Way',
+      address2: '',
+      city: 'Maidstone',
+      county: '',
+      postcode: 'SK22 1DL',
+      country: 'United Kingdom'
+    },
+    businessTelephone: '01234567890',
+    businessMobile: '01234567890',
+    businessEmail: 'a.farmer@farms.com',
+    singleBusinessIdentifier: '123456789',
+    vatNumber: '',
+    tradeNumber: '987654',
+    vendorRegistrationNumber: '699368',
+    countyParishHoldingNumber: '12/563/0998',
+    businessLegalStatus: 'Sole proprietorship',
+    businessType: 'Central or local government',
+    userName: 'Alfred Waldron'
+  }
+}
+
+export {
+  fetchBusinessDetailsService
+}

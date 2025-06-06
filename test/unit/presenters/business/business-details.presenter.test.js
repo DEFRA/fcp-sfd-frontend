353cd945d48244c575bd004ce59aa43b04b391c2
Rebecca Ransome <beckyrose200@aol.com>
2025-06-06 11:55:52 +0100

    Update: Move business telephone and mobile logic into presenter

--- test/unit/presenters/business/business-details.presenter.test.js
+++ test/unit/presenters/business/business-details.presenter.test.js
@@ -8,39 +8,39 @@ describe('businessDetailsPresenter', () => {
   let data
   let yar
 
+  beforeEach(() => {
+    vi.clearAllMocks()
+
+    data = {
+      businessName: 'Agile Farm Ltd',
+      businessAddress: {
+        address1: '10 Skirbeck Way',
+        address2: 'Lonely Lane',
+        city: 'Maidstone',
+        county: 'Somerset',
+        postcode: 'SK22 1DL',
+        country: 'United Kingdom'
+      },
+      businessTelephone: '01234567890',
+      businessMobile: '01234567890',
+      businessEmail: 'a.farmer@farms.com',
+      singleBusinessIdentifier: '123456789',
+      vatNumber: '',
+      tradeNumber: '987654',
+      vendorRegistrationNumber: '699368',
+      countyParishHoldingNumber: '12/563/0998',
+      businessLegalStatus: 'Sole proprietorship',
+      businessType: 'Central or local government',
+      userName: 'Alfred Waldron'
+    }
+
+    // Mock yar session manager
+    yar = {
+      flash: vi.fn().mockReturnValue([{ title: 'Update', text: 'Business details updated successfully' }])
+    }
+  })
+
   describe('when provided with business details data', () => {
-    beforeEach(() => {
-      vi.clearAllMocks()
-
-      data = {
-        businessName: 'Agile Farm Ltd',
-        businessAddress: {
-          address1: '10 Skirbeck Way',
-          address2: 'Lonely Lane',
-          city: 'Maidstone',
-          county: 'Somerset',
-          postcode: 'SK22 1DL',
-          country: 'United Kingdom'
-        },
-        businessTelephone: '01234567890',
-        businessMobile: '01234567890',
-        businessEmail: 'a.farmer@farms.com',
-        singleBusinessIdentifier: '123456789',
-        vatNumber: '',
-        tradeNumber: '987654',
-        vendorRegistrationNumber: '699368',
-        countyParishHoldingNumber: '12/563/0998',
-        businessLegalStatus: 'Sole proprietorship',
-        businessType: 'Central or local government',
-        userName: 'Alfred Waldron'
-      }
-
-      // Mock yar session manager
-      yar = {
-        flash: vi.fn().mockReturnValue([{ title: 'Update', text: 'Business details updated successfully' }])
-      }
-    })
-
     test('it correctly presents the data', () => {
       const result = businessDetailsPresenter(data, yar)
 
@@ -68,21 +68,48 @@ describe('businessDetailsPresenter', () => {
   describe('the "address" property', () => {
     describe('when the address has missing fields', () => {
       beforeEach(() => {
-        data = {
-          businessAddress: {
-            address1: '10 Skirbeck Way',
-            address2: 'Lonely Lane',
-            city: '',
-            county: 'Somerset',
-            postcode: 'SK22 1DL',
-            country: ''
-          }
+        data.businessAddress = {
+          address1: '10 Skirbeck Way',
+          address2: 'Lonely Lane',
+          city: '',
+          county: 'Somerset',
+          postcode: 'SK22 1DL',
+          country: ''
         }
       })
 
       test('it should remove them from the address', () => {
         const result = businessDetailsPresenter(data, yar)
+
         expect(result.address).toEqual(['10 Skirbeck Way', 'Lonely Lane', 'Somerset', 'SK22 1DL'])
+      })
+    })
+  })
+
+  describe('the "businessTelephone" property', () => {
+    describe('when the businessAddress property is missing', () => {
+      beforeEach(() => {
+        data.businessTelephone = null
+      })
+
+      test('it should return the text "Not added', () => {
+        const result = businessDetailsPresenter(data, yar)
+
+        expect(result.businessTelephone).toEqual('Not added')
+      })
+    })
+  })
+
+  describe('the "businessMobile" property', () => {
+    describe('when the businessMobile property is missing', () => {
+      beforeEach(() => {
+        data.businessMobile = null
+      })
+
+      test('it should return the text "Not added', () => {
+        const result = businessDetailsPresenter(data, yar)
+
+        expect(result.businessMobile).toEqual('Not added')
       })
     })
   })

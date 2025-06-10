4e33202e13c8a175a6f8e697cd4e70e9f1d760b4
Rana Salem <ranatasalem@gmail.com>
2025-06-10 15:51:11 +0100

    remove leftover test invalid gql query from example route

--- src/routes/business/example-routes.js
+++ src/routes/business/example-routes.js
@@ -1,32 +1,15 @@
 import { constants as httpConstants } from 'http2'
 import { dalConnector } from '../../dal/connector.js'
-// import { getSbiInfo } from '../../dal/queries/get-sbi-info.js'
+import { getSbiInfo } from '../../dal/queries/get-sbi-info.js'
 
 const email = 'test.user11@defra.gov.uk'
 const variables = { sbi: 107591843 }
-const multiErrorQuery = `
-query Business($sbi: ID!) {
-  business(sbi: $sbi) {
-    sbi
-    info {
-      name
-      email {
-        address
-      }
-      legalStatus {
-        type
-      }}}}}}}
-    }
-  }
-}
-`
-
 
 const exampleDalConnectionRoute = {
   method: 'GET',
   path: '/example',
   handler: async (_request, h) => {
-    const response = await dalConnector(multiErrorQuery, {}, email)
+    const response = await dalConnector(getSbiInfo, variables, email)
 
     if (response.errors) {
       return h.response({

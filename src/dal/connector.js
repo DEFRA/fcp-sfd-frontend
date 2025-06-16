cc2df1b25568d85f0d3b7e83ce7bfe42f2133dec
Rana Salem <ranatasalem@gmail.com>
2025-06-16 10:22:23 +0100

    fix linting errors

--- src/dal/connector.js
+++ src/dal/connector.js
@@ -1,22 +1,18 @@
 import { constants as httpConstants } from 'http2'
 import { createLogger } from '../utils/logger.js'
 import { config } from '../config/index.js'
+import { formatDalResponse, mapDalErrors } from './dal-response.js'
 
 const logger = createLogger()
 
-// TODO: helper for formatting repsonseBody
-
 export const dalConnector = async (query, variables, email) => {
   if (!email) {
-    return {
-      data: null,
+    return formatDalResponse({
       statusCode: httpConstants.HTTP_STATUS_BAD_REQUEST,
-      errors: [
-        {
-          message: 'DAL connection cannot be made if email header is missing'
-        }
-      ]
-    }
+      errors: [{
+        message: 'DAL connection cannot be made if email header is missing'
+      }]
+    })
   }
 
   try {
@@ -32,37 +28,23 @@ export const dalConnector = async (query, variables, email) => {
     const responseBody = await response.json()
 
     if (responseBody.errors) {
-      const extendedErrors = responseBody.errors.map(err => {
-        const ext = err.extensions
-        const parsedMessage = ext?.parsedBody?.message
-        const statusCode = ext?.parsedBody?.statusCode || ext?.response?.status || httpConstants.HTTP_STATUS_BAD_REQUEST
+      const extendedErrors = mapDalErrors(responseBody.errors)
 
-        return {
-          message: parsedMessage ? `${err.message}: ${parsedMessage}` : err.message,
-          statusCode,
-          extensions: ext
-        }
-      })
-
-      return {
-        data: null,
+      return formatDalResponse({
         statusCode: extendedErrors[0]?.statusCode,
         errors: extendedErrors
-      }
+      })
     }
 
-    return {
-      data: responseBody.data,
-      statusCode: httpConstants.HTTP_STATUS_OK,
-      errors: null
-    }
+    return formatDalResponse({
+      data: responseBody.data
+    })
   } catch (err) {
     logger.error(err, 'Error connecting to DAL')
 
-    return {
-      data: null,
+    return formatDalResponse({
       statusCode: httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR,
       errors: [err]
-    }
+    })
   }
 }

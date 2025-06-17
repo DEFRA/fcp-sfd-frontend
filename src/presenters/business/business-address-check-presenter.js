233557cdbdaa207b0f02e836e8a060a7f33e5b32
Rebecca Ransome <beckyrose200@aol.com>
2025-06-17 16:02:04 +0100

    Update: Business address check presenter and test

---
+++ src/presenters/business/business-address-check-presenter.js
@@ -0,0 +1,30 @@
+/**
+ * Formats data ready for presenting in the `/business-address-check` page
+ * @module businessAddressCheckPresenter
+ */
+
+const businessAddressCheckPresenter = (data) => {
+  return {
+    backLink: { href: '/business-address-enter' },
+    cancelLink: '/business-details',
+    changeLink: '/business-address-enter',
+    pageTitle: 'Check your business address is correct before submitting',
+    metaDescription: 'Check the address for your business is correct.',
+    address: formatAddress(data.businessAddress),
+    businessName: data.businessName ?? null,
+    singleBusinessIdentifier: data.singleBusinessIdentifier ?? null,
+    userName: data.userName ?? null
+  }
+}
+
+/**
+ * Formats the business address by removing any falsy values (e.g. empty strings, null, undefined)
+ * @private
+ */
+const formatAddress = (businessAddress) => {
+  return Object.values(businessAddress).filter(Boolean)
+}
+
+export {
+  businessAddressCheckPresenter
+}

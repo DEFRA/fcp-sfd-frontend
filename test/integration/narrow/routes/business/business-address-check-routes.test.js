952e28fee2a09ee70fc68cb4dc559007876e5742
Rebecca Ransome <beckyrose200@aol.com>
2025-06-17 16:06:16 +0100

    Update: business address check routes integration tests

---
+++ test/integration/narrow/routes/business/business-address-check-routes.test.js
@@ -0,0 +1,60 @@
+// Test framework dependencies
+import { describe, test, expect, beforeEach, afterEach, vi } from 'vitest'
+
+describe('business address check routes', () => {
+  const SERVER_MODULE_PATH = '../../../../../src/server.js'
+
+  const resetAndCreateServer = async () => {
+    vi.resetModules()
+
+    const { createServer } = await import(SERVER_MODULE_PATH)
+
+    const server = await createServer()
+    await server.initialize()
+    return server
+  }
+
+  const hookTimeout = 50000
+
+  let server
+
+  beforeEach(async () => {
+    vi.clearAllMocks()
+    server = await resetAndCreateServer()
+  }, hookTimeout)
+
+  afterEach(async () => {
+    await server.stop()
+  }, hookTimeout)
+
+  describe('GET routes', () => {
+    test('when the request succeeds', async () => {
+      // Due to the session cookie being set by the GET route for business
+      // address enter we need to hit that page first
+      const getResponse = await server.inject({ method: 'GET', url: '/business-address-enter' })
+      const cookie = getResponse.headers['set-cookie'][0].split(';')[0] // Extract cookie string
+
+      const response = await server.inject({
+        method: 'GET',
+        url: '/business-address-check',
+        headers: {
+          cookie
+        }
+      })
+
+      expect(response.statusCode).toBe(200)
+      expect(response.payload).contain('Check your business address is correct before submitting')
+    })
+  })
+
+  describe('POST routes', () => {
+    describe('when the request succeeds', () => {
+      test('it redirects to the /business-details page', async () => {
+        const response = await server.inject({ method: 'POST', url: '/business-address-check' })
+
+        expect(response.statusCode).toBe(302)
+        expect(response.headers.location).toBe('/business-details')
+      })
+    })
+  })
+})

bb15e69f7632fea9be85e6be39b27021ccff90fe
Rebecca Ransome <beckyrose200@aol.com>
2025-06-19 14:37:02 +0100

    Update: Business Name Change Presenter and test

--- src/presenters/business/business-name-change-presenter.js
+++ src/presenters/business/business-name-change-presenter.js
@@ -8,9 +8,12 @@ const businessNameChangePresenter = (data, payload) => {
         backLink: { href: '/business-details' },
         pageTitle: 'What is your business name?',
         metaDescription: 'Update the name for your business.',
-        businessName: data.businessName ?? null,
-        sbi: data.sbi ?? null,
-        userName: data.userName ?? null
+        businessName: payload ?? data.businessName,
+        subHeader: {
+            businessName: data.businessName ?? null,
+            sbi: data.sbi ?? null,
+            userName: data.userName ?? null
+        }
     }
 }
 

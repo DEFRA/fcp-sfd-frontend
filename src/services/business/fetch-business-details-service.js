2dfff88fc76de2eb31fbfd6b26426fb139cdff1d
Fay Toward <fay.toward@defra.gov.uk>
2025-06-25 16:38:35 +0100

    spacing fix

--- src/services/business/fetch-business-details-service.js
+++ src/services/business/fetch-business-details-service.js
@@ -11,27 +11,27 @@ const fetchBusinessDetailsService = async (yar) => {
   const businessDetails = yar.get('businessDetails')
     ? yar.get('businessDetails')
     : {
-        businessName: 'Agile Farm Ltd',
-        businessAddress: {
-          address1: '10 Skirbeck Way',
-          address2: '',
-          city: 'Maidstone',
-          county: '',
-          postcode: 'SK22 1DL',
-          country: 'United Kingdom'
-        },
-        businessTelephone: '01234567890',
-        businessMobile: '01234567890',
-        businessEmail: 'a.farmer@farms.com',
-        sbi: '123456789',
-        vatNumber: '',
-        tradeNumber: '987654',
-        vendorRegistrationNumber: '699368',
-        countyParishHoldingNumber: '12/563/0998',
-        businessLegalStatus: 'Sole proprietorship',
-        businessType: 'Central or local government',
-        userName: 'Alfred Waldron'
-      }
+      businessName: 'Agile Farm Ltd',
+      businessAddress: {
+        address1: '10 Skirbeck Way',
+        address2: '',
+        city: 'Maidstone',
+        county: '',
+        postcode: 'SK22 1DL',
+        country: 'United Kingdom'
+      },
+      businessTelephone: '01234567890',
+      businessMobile: '01234567890',
+      businessEmail: 'a.farmer@farms.com',
+      sbi: '123456789',
+      vatNumber: '',
+      tradeNumber: '987654',
+      vendorRegistrationNumber: '699368',
+      countyParishHoldingNumber: '12/563/0998',
+      businessLegalStatus: 'Sole proprietorship',
+      businessType: 'Central or local government',
+      userName: 'Alfred Waldron'
+    }
   yar.set('businessDetails', businessDetails)
   return businessDetails
 }

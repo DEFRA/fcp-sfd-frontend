edf7725613a171a9ef6e45ef25aac6e2ef227597
Rebecca Ransome <beckyrose200@aol.com>
2025-07-13 17:43:07 +0100

    Update: Update business name change service and test

--- src/services/business/update-business-name-change-service.js
+++ src/services/business/update-business-name-change-service.js
@@ -7,10 +7,13 @@ import { fetchBusinessDetailsService } from './fetch-business-details-service.js
 import { flashNotification } from '../../utils/notifications/flash-notification.js'
 
 const updateBusinessNameChangeService = async (yar) => {
-  await fetchBusinessDetailsService(yar)
-  const businessDetails = yar.get('businessDetails')
+  const businessDetails = await fetchBusinessDetailsService(yar)
+
   businessDetails.info.businessName = businessDetails.changeBusinessName
+  delete businessDetails.changeBusinessName
+
   yar.set('businessDetails', businessDetails)
+
   flashNotification(yar, 'Success', 'You have updated your business name')
 }
 
--- test/unit/services/business/update-business-name-change-service.test.js
+++ test/unit/services/business/update-business-name-change-service.test.js
@@ -1,10 +1,18 @@
+// Test framework dependencies
 import { describe, test, expect, beforeEach, vi } from 'vitest'
-import { updateBusinessNameChangeService } from '../../../../src/services/business/update-business-name-change-service.js'
-import { fetchBusinessDetailsService } from '../../../../src/services/business/fetch-business-details-service.js'
+
+// Things we need to mock
+import { fetchBusinessDetailsService } from '../../../../src/services/business/fetch-business-details-service'
 import { flashNotification } from '../../../../src/utils/notifications/flash-notification.js'
+
+// Test helpers
 import { mappedData } from '../../../mocks/mock-business-details.js'
 
-vi.mock('../../../../src/services/business/fetch-business-details-service.js', () => ({
+// Thing under test
+import { updateBusinessNameChangeService } from '../../../../src/services/business/update-business-name-change-service'
+
+// Mocks
+vi.mock('../../../../src/services/business/fetch-business-details-service', () => ({
   fetchBusinessDetailsService: vi.fn()
 }))
 
@@ -13,26 +21,31 @@ vi.mock('../../../../src/utils/notifications/flash-notification.js', () => ({
 }))
 
 describe('updateBusinessNameChangeService', () => {
-  let data
   let yar
 
   beforeEach(() => {
     vi.clearAllMocks()
 
-    data = mappedData
+    mappedData.changeBusinessName = 'New business ltd'
+    fetchBusinessDetailsService.mockReturnValue(mappedData)
+
     yar = {
-      get: vi.fn().mockReturnValue(data),
-      set: vi.fn().mockReturnValue(data)
+      set: vi.fn().mockReturnValue()
     }
   })
 
   describe('when called', () => {
-    test('it correctly returns the data', async () => {
+    test('it correctly saves the data to the session', async () => {
       await updateBusinessNameChangeService(yar)
+
       expect(fetchBusinessDetailsService).toHaveBeenCalled(yar)
-      expect(flashNotification).toHaveBeenCalled()
-      expect(yar.get).toHaveBeenCalledWith('businessDetails')
-      expect(yar.set).toHaveBeenCalledWith('businessDetails', data)
+      expect(yar.set).toHaveBeenCalledWith('businessDetails', mappedData)
+    })
+
+    test('adds a flash notification confirming the change in data', async () => {
+      await updateBusinessNameChangeService(yar)
+
+      expect(flashNotification).toHaveBeenCalledWith(yar, 'Success', 'You have updated your business name')
     })
   })
 })

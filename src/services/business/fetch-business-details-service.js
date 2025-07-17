d0ccea37fa2c4acb1559418ed94b90ac8bf4b9a6
Rebecca Ransome <beckyrose200@aol.com>
2025-07-17 17:29:50 +0100

    Update: Fetch business details service to use feature toggle

--- src/services/business/fetch-business-details-service.js
+++ src/services/business/fetch-business-details-service.js
@@ -3,14 +3,17 @@
  * otherwise queries the DAL, maps the response, updates the cache
  * and returns the mapped payload
  *
+ * Note: In some environments the DAL connection is disabled while we wait for the DAL team to set it up, specifically
+ * test and pre
+ *
  * @module fetchBusinessDetailsService
  */
 
 import { dalConnector } from '../../dal/connector.js'
 import { businessDetailsQuery } from '../../dal/queries/business-details.js'
 import { mapBusinessDetails } from '../../mappers/business-details-mapper.js'
-import { dalData } from '../../../test/mocks/mock-business-details.js'
-import { config } from '../../config/index.js'
+import { mappedData } from '../../../test/mocks/mock-business-details.js'
+import { featureFlagsConfig } from '../../config/feature-flags.js'
 
 const fetchBusinessDetailsService = async (yar) => {
   const businessDetails = yar.get('businessDetails')
@@ -19,33 +22,27 @@ const fetchBusinessDetailsService = async (yar) => {
     return businessDetails
   }
 
-  // If the DAL connection is not set then return the mock data
-  if (!config.get('server.dalConnection')) {
-    const mappedResponse = mapBusinessDetails(dalData)
-    yar.set('businessDetails', mappedResponse)
+  // If the DAL connection is set to false return mock data otherwise query the DAL
+  const businessDetailsData = !featureFlagsConfig.dalConnection ? mappedData : await getFromDal(yar)
 
-    return mappedResponse
-  }
+  // Set the business details data on the session
+  yar.set('businessDetails', businessDetailsData)
 
-  // Return the response from the DAL
-  return getFromDal(yar)
+  return businessDetailsData
 }
 
 const getFromDal = async (yar) => {
-  // replace variables and email when defraId is setup
+  // Replace variables and email when defraId is setup
   const variables = { sbi: '107183280', crn: '9477368292' }
   const email = 'not-a-real-email@test.co.uk'
 
   const dalResponse = await dalConnector(businessDetailsQuery, variables, email)
 
-  if (dalResponse.data) {
-    const mappedResponse = mapBusinessDetails(dalResponse.data)
-    yar.set('businessDetails', mappedResponse)
-
-    return mappedResponse
+  if (!dalResponse.data) {
+    return dalResponse
   }
 
-  return dalResponse
+  return mapBusinessDetails(dalResponse.data)
 }
 
 export {

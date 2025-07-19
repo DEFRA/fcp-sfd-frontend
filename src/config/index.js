ac010dcd49a7fc669f8ae69b9e32c62bef41dddf
Abidemi Adio <abidemi.adio@defra.gov.uk>
2025-07-19 15:33:31 +0100

    Added feature Toggle config

--- src/config/index.js
+++ src/config/index.js
@@ -4,13 +4,15 @@ import { nunjucksConfig } from './nunjucks.js'
 import { redisConfig } from './redis.js'
 import { defraIdConfig } from './defra-id.js'
 import { dalConfig } from './dal.js'
+import { featureToggleConfig } from './feature-toggle.js'
 
 const config = convict({
   ...serverConfig,
   ...nunjucksConfig,
   ...redisConfig,
   ...defraIdConfig,
-  ...dalConfig
+  ...dalConfig,
+  ...featureToggleConfig
 })
 
 config.validate({ allowed: 'strict' })

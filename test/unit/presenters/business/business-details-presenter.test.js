e3d712e4b9686196cfd430eef338ccdbf82dee3e
Fay Toward <fay.toward@defra.gov.uk>
2025-07-21 14:37:14 +0100

    4 failing tests

--- test/unit/presenters/business/business-details-presenter.test.js
+++ test/unit/presenters/business/business-details-presenter.test.js
@@ -45,7 +45,7 @@ describe('businessDetailsPresenter', () => {
         vatNumber: data.info.vat,
         tradeNumber: data.info.traderNumber,
         vendorRegistrationNumber: data.info.vendorNumber,
-        countyParishHoldingNumber: '12/123/1234',
+        countyParishHoldingNumbers: ['12/123/1234'],
         businessLegalStatus: data.info.legalStatus,
         businessType: data.info.type,
         userName: data.customer.fullName

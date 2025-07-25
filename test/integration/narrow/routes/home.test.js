64d325d856220712f38cefbda9510cb92a174f8b
Abidemi Adio <abidemi.adio@defra.gov.uk>
2025-07-25 02:47:36 +0100

    Added integration test

---
+++ test/integration/narrow/routes/home.test.js
@@ -0,0 +1,50 @@
+import { constants } from 'http2'
+import { vi, beforeAll, afterAll, describe, test, expect } from 'vitest'
+import '../../../mocks/setup-server-mocks.js'
+
+const { HTTP_STATUS_OK, HTTP_STATUS_FOUND } = constants
+
+const { createServer } = await import('../../../../src/server.js')
+
+let server
+
+const path = '/home'
+
+describe('home route', () => {
+  beforeAll(async () => {
+    vi.clearAllMocks()
+
+    server = await createServer()
+    await server.initialize()
+  })
+
+  afterAll(async () => {
+    if (server) {
+      await server.stop()
+    }
+  })
+
+  test('GET /home returns index view if authenticated and has "user" scope', async () => {
+    const response = await server.inject({
+      url: path,
+      auth: {
+        strategy: 'session',
+        credentials: {
+          sessionId: 'session-id',
+          scope: ['BUSINESS_DETAILS:FULL_PERMISSION']
+        }
+      }
+    })
+    expect(response.statusCode).toBe(HTTP_STATUS_OK)
+    expect(response.request.response.source.template).toBe('home')
+  })
+
+  test('GET /home redirects to /auth/sign-in if not authenticated', async () => {
+    const response = await server.inject({
+      method: 'GET',
+      url: '/home'
+    })
+    expect(response.statusCode).toBe(HTTP_STATUS_FOUND)
+    expect(response.headers.location).toBe('/auth/sign-in?redirect=/home')
+  })
+})

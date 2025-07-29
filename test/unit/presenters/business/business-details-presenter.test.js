73979fa52312c8e28fa873ac6f399a61dcc80193
Abidemi Adio <abidemi.adio@defra.gov.uk>
2025-07-29 21:00:23 +0100

    Fixed display and unit test issues

--- test/unit/presenters/business/business-details-presenter.test.js
+++ test/unit/presenters/business/business-details-presenter.test.js
@@ -45,7 +45,7 @@ describe('businessDetailsPresenter', () => {
         vatNumber: data.info.vat,
         tradeNumber: data.info.traderNumber,
         vendorRegistrationNumber: data.info.vendorNumber,
-        countyParishHoldingNumbers: '12/123/1234<br>',
+        countyParishHoldingNumbers: ['12/123/1234'],
         businessLegalStatus: data.info.legalStatus,
         businessType: data.info.type,
         userName: data.customer.fullName

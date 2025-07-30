7ccd95909cf8e6f174434922565470a3db3bdbfd
Rebecca Ransome <beckyrose200@aol.com>
2025-07-30 09:11:18 +0100

    Add: Base presenter and tests

---
+++ test/unit/presenters/base-presenter.test.js
@@ -0,0 +1,103 @@
+// Test framework dependencies
+import { describe, test, expect, beforeEach } from 'vitest'
+
+// Thing under test
+import { basePresenter } from '../../../src/presenters/base-presenter.js'
+
+describe('base presenter', () => {
+  describe('#formatAddress', () => {
+    let address
+
+    beforeEach(async () => {
+      address = {
+        lookup: {
+          flatName: 'THE COACH HOUSE',
+          buildingNumberRange: '7',
+          buildingName: 'STOCKWELL HALL',
+          street: 'HAREWOOD AVENUE',
+          city: 'DARLINGTON',
+          county: 'Dorset'
+        },
+        manual: {
+          line1: '76 Robinswood Road',
+          line2: 'UPPER CHUTE',
+          line3: 'Child Okeford',
+          line4: null,
+          line5: null
+        },
+        postcode: 'CO9 3LS',
+        country: 'United Kingdom'
+      }
+    })
+
+    describe('when the address has line properties and named properties', () => {
+      test('it should use the named properties ', () => {
+        const result = basePresenter.formatAddress(address)
+
+        expect(result).toStrictEqual([
+          'THE COACH HOUSE',
+          'STOCKWELL HALL',
+          '7 HAREWOOD AVENUE',
+          'DARLINGTON',
+          'Dorset',
+          'CO9 3LS',
+          'United Kingdom'
+        ])
+      })
+    })
+
+    describe('when the named properties include a building number', () => {
+      test('it should prefix the street with the number', () => {
+        const result = basePresenter.formatAddress(address)
+
+        expect(result).toStrictEqual([
+          'THE COACH HOUSE',
+          'STOCKWELL HALL',
+          '7 HAREWOOD AVENUE',
+          'DARLINGTON',
+          'Dorset',
+          'CO9 3LS',
+          'United Kingdom'
+        ])
+      })
+    })
+
+    describe('when the named properties does not have a building number', () => {
+      test('it should leave the street property unchanged', () => {
+        address.lookup.buildingNumberRange = null
+        const result = basePresenter.formatAddress(address)
+
+        expect(result).toStrictEqual([
+          'THE COACH HOUSE',
+          'STOCKWELL HALL',
+          'HAREWOOD AVENUE',
+          'DARLINGTON',
+          'Dorset',
+          'CO9 3LS',
+          'United Kingdom'
+        ])
+      })
+    })
+
+    describe('when the address has no named properties', () => {
+      test('it should use the lined properties ', () => {
+        address.lookup.flatName = null
+        address.lookup.buildingNumberRange = null
+        address.lookup.buildingName = null
+        address.lookup.street = null
+        address.lookup.city = null
+        address.lookup.county = null
+
+        const result = basePresenter.formatAddress(address)
+
+        expect(result).toEqual([
+          '76 Robinswood Road',
+          'UPPER CHUTE',
+          'Child Okeford',
+          'CO9 3LS',
+          'United Kingdom'
+        ])
+      })
+    })
+  })
+})

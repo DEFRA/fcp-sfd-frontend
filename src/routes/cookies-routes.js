9a361f7dd3c5e7a77fcfd796c60b01738e0913b6
Abidemi Adio <abidemi.adio@defra.gov.uk>
2025-08-01 09:19:53 +0100

    Made cookies page backLink dynamic

--- src/routes/cookies-routes.js
+++ src/routes/cookies-routes.js
@@ -2,9 +2,11 @@ export const cookies = {
   method: 'GET',
   path: '/cookies',
   handler: (_request, h) => {
+    backLink = request.headers.referer
     return h.view('cookies', {
       pageTitle: 'Cookies',
-      heading: 'How we use cookies to store information about how you use this service.'
+      heading: 'How we use cookies to store information about how you use this service.',
+      backLink
     })
   }
 }

ac522877dc54fe5abe21dafab5a2597f55ed06fa
Fay Toward <fay.toward@defra.gov.uk>
2025-08-04 13:38:39 +0100

    backlink change made

--- src/routes/footer/accessibility-statement-routes.js
+++ src/routes/footer/accessibility-statement-routes.js
@@ -2,9 +2,11 @@ export const accessibilityStatement = {
   method: 'GET',
   path: '/accessibility-statement',
   handler: (_request, h) => {
+    backLink = request.headers.referer
     return h.view('footer/accessibility-statement', {
       pageTitle: 'Accessibility statement',
-      heading: 'Accessibility statement'
+      heading: 'Accessibility statement',
+      backLink
     })
   }
 }

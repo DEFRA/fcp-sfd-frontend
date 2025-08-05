5822cf8bca3ef9e10d6012e3271a7fa95bb79cab
AbidemiAdio <98023713+AbidemiAdio@users.noreply.github.com>
2025-08-05 13:44:46 +0100

    Added signed-out page (#86)
    * Added signed-out page
    
    * Modified code IRO PR FB
    
    * Confirm tests passed locally
    
    * Added more comment IRO PR FB
    
    * Updated version Number

---
+++ test/integration/narrow/routes/signed-out-routes.test.js
@@ -0,0 +1,33 @@
+import { describe, test, expect, beforeAll, afterAll } from 'vitest'
+import * as cheerio from 'cheerio'
+import { createServer } from '../../../../src/server.js'
+
+describe('signed out route integration', async () => {
+  let server
+  let response
+
+  beforeAll(async () => {
+    server = await createServer()
+    await server.initialize()
+    response = await server.inject({
+      method: 'GET',
+      url: '/signed-out'
+    })
+  })
+
+  afterAll(async () => {
+    await server.stop()
+  })
+
+  test('signed out route is registered', async () => {
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('Ensure that page title is "You have signed out"', async () => {
+    const $ = cheerio.load(response.result)
+    const mainContent = $('#main-content')
+    const pageTitle = mainContent.find('h1').text()
+
+    expect(pageTitle).toEqual('You have signed out')
+  })
+})

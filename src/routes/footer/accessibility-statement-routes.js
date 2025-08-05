2bb3ba9b850a82c29c5e78c7a32319457c6470d9
FayToward <43171822+FayToward@users.noreply.github.com>
2025-08-05 09:28:38 +0100

    Backlink accessibility statement (#87)
    https://eaflood.atlassian.net/browse/SFD-75

--- src/routes/footer/accessibility-statement-routes.js
+++ src/routes/footer/accessibility-statement-routes.js
@@ -1,10 +1,12 @@
 export const accessibilityStatement = {
   method: 'GET',
   path: '/accessibility-statement',
-  handler: (_request, h) => {
+  handler: (request, h) => {
+    const backLink = request.headers.referer
     return h.view('footer/accessibility-statement', {
       pageTitle: 'Accessibility statement',
-      heading: 'Accessibility statement'
+      heading: 'Accessibility statement',
+      backLink
     })
   }
 }

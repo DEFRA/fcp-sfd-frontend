1f1594543e0d5d8ce3640323a857be8ecfcc791e
Fay Toward <fay.toward@defra.gov.uk>
2025-08-05 08:55:18 +0100

    test fix

--- test/unit/routes/accessibility-statement-routes.test.js
+++ test/unit/routes/accessibility-statement-routes.test.js
@@ -1,14 +1,11 @@
 import { vi, beforeEach, describe, test, expect } from 'vitest'
 import { accessibilityStatement } from '../../../src/routes/footer/accessibility-statement-routes'
 
-const mockView = vi.fn()
-
-const mockH = {
-  view: vi.fn().mockReturnValue(mockView)
-}
-
 describe('Accessibility statement endpoint', () => {
+  let viewMock
+
   beforeEach(() => {
+    viewMock = vi.fn().mockReturnValue('mock view return')
     vi.clearAllMocks()
   })
 
@@ -17,14 +14,23 @@ describe('Accessibility statement endpoint', () => {
     expect(accessibilityStatement.path).toBe('/accessibility-statement')
   })
 
-  test('should render the accessibility-statment view with correct data', () => {
-    const result = accessibilityStatement.handler(null, mockH)
+  test('should render the accessibility-statement view with correct data', () => {
+    const mockRequest = {
+      headers: {
+        referer: '/some-previous-page'
+      }
+    }
 
-    expect(mockH.view).toHaveBeenCalledWith('footer/accessibility-statement', {
+    const h = { view: viewMock }
+
+    const result = accessibilityStatement.handler(mockRequest, h)
+
+    expect(viewMock).toHaveBeenCalledWith('footer/accessibility-statement', {
       pageTitle: 'Accessibility statement',
-      heading: 'Accessibility statement'
+      heading: 'Accessibility statement',
+      backLink: '/some-previous-page'
     })
 
-    expect(result).toBe(mockView)
+    expect(result).toBe('mock view return')
   })
 })

d891c22f4ec7c7dbf59b62c2d7ac885473f926b3
Rebecca Ransome <beckyrose200@aol.com>
2025-08-11 12:15:01 +0100

    Update: route files to use new naming convention

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,10 @@
-import { errors } from './errors/index-routes.js'
+import { errors } from './errors/error-routes.js'
 import { health } from './health-routes.js'
 import { home } from './home-routes.js'
 import { staticAssetRoutes } from './static-assets-routes.js'
 import { cookies } from './cookies-routes.js'
 import { signedOut } from './signed-out-routes.js'
-import { footerRoutes } from './footer/index-routes.js'
+import { footerRoutes } from './footer/footer-routes.js'
 import { businessRoutes } from './business/business-routes.js'
 import { personalRoutes } from './personal/personal-routes.js'
 

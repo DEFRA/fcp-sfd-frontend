baf4a2643317287f2d0f1369a3b3de8890474447
Abidemi Adio <abidemi.adio@defra.gov.uk>
2025-08-15 02:34:51 +0100

    added more trace log

--- src/auth/get-oidc-config.js
+++ src/auth/get-oidc-config.js
@@ -1,12 +1,15 @@
 import Wreck from '@hapi/wreck'
 import { config } from '../config/index.js'
+import { createLogger } from '../utils/logger.js'
 
 async function getOidcConfig () {
+  const logger = createLogger()
   try {
     const { payload } = await Wreck.get(config.get('defraId.wellKnownUrl'), {
       json: true
     })
-
+    logger.info('got pay load here')
+    logger.info(payload)
     return payload
   } catch (err) {
     throw new Error('Unable to connect to DefraId ' + config.get('defraId.wellKnownUrl') + ' ' + err.message)

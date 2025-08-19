0049450fedc6ce4970fbc28d9797e706486438fa
Rebecca Ransome <beckyrose200@aol.com>
2025-08-19 11:29:24 +0100

    Fix: get-permissions-test file

--- test/unit/auth/get-permissions.test.js
+++ test/unit/auth/get-permissions.test.js
@@ -28,6 +28,7 @@ const { getPermissions } = await import('../../../src/auth/get-permissions.js')
 let sbi
 let crn
 let email
+let tokenCache
 
 describe('getPermissions', () => {
   beforeEach(() => {
@@ -36,6 +37,7 @@ describe('getPermissions', () => {
     sbi = '234654278765'
     crn = '987645433252'
     email = 'farmer@test.com'
+    tokenCache = 'test-token'
 
     mockConfigGet.mockReturnValue(true)
     mapPermissions.mockReturnValue(mappedData)
@@ -47,29 +49,29 @@ describe('getPermissions', () => {
       mockConfigGet.mockReturnValue(true)
     })
     test('should call dalConnector with getPermission parameters', async () => {
-      await getPermissions(sbi, crn, email)
-      expect(dalConnector).toHaveBeenCalledWith(permissionsQuery, { sbi, crn }, email)
+      await getPermissions(sbi, crn, email, tokenCache)
+      expect(dalConnector).toHaveBeenCalledWith(permissionsQuery, { sbi, crn }, email, tokenCache)
     })
     test('should call mapPermissions when dalConnector response has data', async () => {
-      await getPermissions(sbi, crn, email)
+      await getPermissions(sbi, crn, email, tokenCache)
       expect(mapPermissions).toHaveBeenCalledWith(dalData)
     })
 
     test('should return mapped data  when dalConnector response has data', async () => {
-      const result = await getPermissions(sbi, crn, email)
+      const result = await getPermissions(sbi, crn, email, tokenCache)
       expect(result).toBe(mappedData)
     })
 
     test('should not call mapPermissions when dalConnector response has no data', async () => {
       dalConnector.mockResolvedValue({ })
-      await getPermissions(sbi, crn, email)
+      await getPermissions(sbi, crn, email, tokenCache)
       expect(mapPermissions).not.toHaveBeenCalled()
     })
 
     test('should return dalConnector response when dalConnector response has no data', async () => {
       const dalResponse = { response: 'no-dal-data' }
       dalConnector.mockResolvedValue(dalResponse)
-      const result = await getPermissions(sbi, crn, email)
+      const result = await getPermissions(sbi, crn, email, tokenCache)
       expect(result).toBe(dalResponse)
     })
   })
@@ -79,12 +81,12 @@ describe('getPermissions', () => {
       mockConfigGet.mockReturnValue(false)
     })
     test('dalConnector is not called', async () => {
-      await getPermissions(sbi, crn, email)
+      await getPermissions(sbi, crn, email, tokenCache)
       expect(dalConnector).not.toHaveBeenCalled()
     })
 
     test('it correctly returns data static data source', async () => {
-      const result = await getPermissions(sbi, crn, email)
+      const result = await getPermissions(sbi, crn, email, tokenCache)
       expect(result).toMatchObject(mappedData)
     })
   })

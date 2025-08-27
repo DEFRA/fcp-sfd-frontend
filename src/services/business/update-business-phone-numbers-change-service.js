14b271bc9641c810de2ab2fae5e8298da4d89c5b
Rebecca Ransome <beckyrose200@aol.com>
2025-08-27 17:14:19 +0100

    Update: update business phone numbers change service

--- src/services/business/update-business-phone-numbers-change-service.js
+++ src/services/business/update-business-phone-numbers-change-service.js
@@ -1,9 +1,13 @@
+import { dalConnector } from '../../dal/connector.js'
+import { updateBusinessPhoneMutation } from '../../dal/mutations/update-business-phone-numbers.js'
 import { fetchBusinessDetailsService } from './fetch-business-details-service.js'
 import { flashNotification } from '../../utils/notifications/flash-notification.js'
 
 const updateBusinessPhoneNumbersChangeService = async (yar, credentials) => {
   const businessDetails = await fetchBusinessDetailsService(yar, credentials)
+  await updateBusinessPhone(businessDetails)
 
+  // Update the cached data to now reflect the real data
   businessDetails.contact.landline = businessDetails.changeBusinessTelephone ?? null
   businessDetails.contact.mobile = businessDetails.changeBusinessMobile ?? null
 
@@ -13,6 +17,24 @@ const updateBusinessPhoneNumbersChangeService = async (yar, credentials) => {
   yar.set('businessDetails', businessDetails)
 
   flashNotification(yar, 'Success', 'You have updated your business phone numbers')
+}
+
+const updateBusinessPhone = async (businessDetails) => {
+  const variables = {
+    input: {
+      phone: {
+        landline: businessDetails.changeBusinessTelephone ?? null,
+        mobile: businessDetails.changeBusinessMobile ?? null
+      },
+      sbi: businessDetails.info.sbi
+    }
+  }
+
+  const response = await dalConnector(updateBusinessPhoneMutation, variables)
+
+  if (response.errors) {
+    throw new Error('DAL error from mutation')
+  }
 }
 
 export {

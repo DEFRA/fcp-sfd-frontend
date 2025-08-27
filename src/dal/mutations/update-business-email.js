26ab5a7f88b5aa06ffbe74a00f03793d4bf6dd5d
Adam Kay <thousand_leaves@hotmail.com>
2025-08-27 13:43:13 +0100

    Remove: question in comment

--- src/dal/mutations/update-business-email.js
+++ src/dal/mutations/update-business-email.js
@@ -12,20 +12,3 @@ export const updateBusinessEmailMutation = `
     }
   }
 `
-
-// there is a validated variable (presumed to be a boolean) that we may want to use e.g.
-// updateBusinessEmailMutation = `
-//   mutation Mutation($input: UpdateBusinessEmailInput!) {
-//     updateBusinessEmail(input: $input) {
-//       business {
-//         info {
-//           email {
-//             address
-//             validated
-//           }
-//         }
-//       }
-//       success
-//     }
-//   }
-// `

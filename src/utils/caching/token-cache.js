43b6c2074de1a77165792d64f6be0345339db9f9
Simon Dunn <simondunn90@gmail.com>
2025-08-28 16:39:07 +0100

    tidy

--- src/utils/caching/token-cache.js
+++ src/utils/caching/token-cache.js
@@ -3,11 +3,6 @@ import { config } from '../../config/index.js'
 let tokenCache = null
 
 export const initTokenCache = (server, cacheName) => {
-  console.log('creating token cache')
-  console.log(cacheName)
-  console.log(config.get('server.session.cache.tokenSegment'))
-  console.log(config.get('redis.ttl'))
-
   tokenCache = server.cache({
     cache: cacheName,
     segment: config.get('server.session.cache.tokenSegment'),

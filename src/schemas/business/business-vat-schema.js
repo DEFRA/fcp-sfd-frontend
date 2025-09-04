b8d42d4b67e1b9923d97323be94e2b18d988b307
Fay Toward <fay.toward@defra.gov.uk>
2025-09-04 14:49:43 +0100

    vat tests

--- src/schemas/business/business-vat-schema.js
+++ src/schemas/business/business-vat-schema.js
@@ -3,9 +3,10 @@ import Joi from 'joi'
 export const businessVatSchema = Joi.object({
   vatNumber: Joi.string()
     .pattern(/^\d{9}$/)
+    .required()
     .messages({
       'string.pattern.base': 'Enter a VAT registration number in the format 123456789',
-      'string.empty': 'Enter a VAT registration number'
+      'string.empty': 'Enter a VAT registration number',
+      'any.required': 'Enter a VAT registration number' // 👈 Add this line
     })
-    .required()
 })

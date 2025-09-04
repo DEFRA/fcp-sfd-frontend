e428cd540bd8381aefe806322fdd63112bae524f
Adam Kay <thousand_leaves@hotmail.com>
2025-09-04 15:22:50 +0100

    Update: revert schema

--- src/schemas/business/business-email-schema.js
+++ src/schemas/business/business-email-schema.js
@@ -3,20 +3,18 @@ import { EMAIL_MAX } from '../../constants/validation-fields.js'
 
 export const businessEmailSchema = Joi.object({
   businessEmail: Joi.string()
+    .required()
     .max(EMAIL_MAX)
-    .when(Joi.string().max(EMAIL_MAX), {
-      then: Joi.string().email({
-        minDomainSegments: 2,
-        tlds: {
-          allow: true,
-          min: 2
-        }
-      })
+    .email({
+      minDomainSegments: 2,
+      tlds: {
+        allow: true,
+        min: 2
+      }
     })
     .messages({
-      'string.empty': 'Enter business email address',
       'string.max': `Business email address must be ${EMAIL_MAX} characters or less`,
+      'string.empty': 'Enter business email address',
       'string.email': 'Enter an email address, like name@example.com'
     })
-    .required()
 })

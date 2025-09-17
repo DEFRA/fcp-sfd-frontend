821dd5c99b935dd36e3f0e2501a151c37e4cb2f5
Rebecca Ransome <beckyrose200@aol.com>
2025-09-17 15:43:24 +0100

    Update: Update business vat change service and test

--- src/services/business/update-business-vat-change-service.js
+++ src/services/business/update-business-vat-change-service.js
@@ -1,22 +1,27 @@
-import { dalConnector } from '../../dal/connector.js'
+/**
+ * Service to update a business's VAT registration number
+ *
+ * Fetches the pending VAT change from the session
+ * Calls the DAL to persist the updated VAT number using updateDalService
+ * Clears the cached business details data from the session
+ * Displays a success flash notification to the user
+ *
+ * @module updateBusinessVatChangeService
+ */
+
+import { fetchBusinessChangeService } from './fetch-business-change-service.js'
 import { updateBusinessVATMutation } from '../../dal/mutations/update-business-vat.js'
-import { fetchBusinessDetailsService } from './fetch-business-details-service.js'
 import { flashNotification } from '../../utils/notifications/flash-notification.js'
+import { updateDalService } from '../DAL/update-dal-service.js'
 
 const updateBusinessVatChangeService = async (yar, credentials) => {
-  const businessDetails = await fetchBusinessDetailsService(yar, credentials)
+  const businessDetails = await fetchBusinessChangeService(yar, credentials, 'changeBusinessVAT')
+  const variables = { input: { vat: businessDetails.changeBusinessVAT, sbi: businessDetails.info.sbi } }
 
-  const variables = { input: { vat: businessDetails.changeBusinessVat, sbi: businessDetails.info.sbi } }
-  const response = await dalConnector(updateBusinessVATMutation, variables)
+  await updateDalService(updateBusinessVATMutation, variables)
 
-  if (response.errors) {
-    throw new Error('DAL error from mutation')
-  }
+  yar.clear('businessDetails')
 
-  businessDetails.info.vat = businessDetails.changeBusinessVat
-  delete businessDetails.changeBusinessVat
-
-  yar.set('businessDetails', businessDetails)
   flashNotification(yar, 'Success', 'You have updated your VAT registration number')
 }
 

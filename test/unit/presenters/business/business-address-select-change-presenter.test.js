9f20fc398ca64a1b245bedbc48fd0f45dbb4ae28
Rebecca Ransome <beckyrose200@aol.com>
2025-09-25 15:48:12 +0100

    Add: Business address select change presenter and test

---
+++ test/unit/presenters/business/business-address-select-change-presenter.test.js
@@ -0,0 +1,126 @@
+// Test framework dependencies
+import { describe, test, expect, beforeEach } from 'vitest'
+
+// Thing under test
+import { businessAddressSelectChangePresenter } from '../../../../src/presenters/business/business-address-select-change-presenter.js'
+
+describe('businessAddressSelectChangePresenter', () => {
+  let data
+
+  beforeEach(() => {
+    data = {
+      info: {
+        businessName: 'Agile Farm Ltd',
+        sbi: '123456789'
+      },
+      customer: {
+        fullName: 'Alfred Waldron'
+      },
+      changeBusinessPostcode: {
+        businessPostcode: 'SK22 1DL',
+      },
+      changeBusinessAddresses: [
+        {
+          uprn: '100000111111',
+          displayAddress: 'Flat 3, Fake Court, 18, Maple Road, Westfield, Bristol, BS1 4AB',
+        },
+        {
+          uprn: '100000222222',
+          displayAddress: 'TechWorks Ltd, Customer Services, Innovation House, 42, Harbour Street, Manchester, M1 7YZ',
+        }
+      ]
+    }
+  })
+
+  describe('when provided with business address select change data', () => {
+    test('it correctly presents the data', () => {
+      const result = businessAddressSelectChangePresenter(data)
+
+      expect(result).toEqual({
+        backLink: { href: '/business-address-change' },
+        pageTitle: 'Choose your business address',
+        metaDescription: 'Choose the address for your business.',
+        postcode: 'SK22 1DL',
+        businessName: 'Agile Farm Ltd',
+        sbi: '123456789',
+        userName: 'Alfred Waldron',
+        displayAddresses: [
+          {
+            value: 'display',
+            text: '2 addresses found',
+            selected: true
+          },
+          {
+            value: '100000111111Flat 3, Fake Court, 18, Maple Road, Westfield, Bristol, BS1 4AB',
+            text: 'Flat 3, Fake Court, 18, Maple Road, Westfield, Bristol, BS1 4AB',
+            selected: false
+          },
+          {
+            value: '100000222222TechWorks Ltd, Customer Services, Innovation House, 42, Harbour Street, Manchester, M1 7YZ',
+            text: 'TechWorks Ltd, Customer Services, Innovation House, 42, Harbour Street, Manchester, M1 7YZ',
+            selected: false
+          }
+        ]
+      })
+    })
+  })
+
+  describe('the "businessName" property', () => {
+    describe('when the businessName property is missing', () => {
+      beforeEach(() => {
+        delete data.info.businessName
+      })
+
+      test('it should return businessName as null', () => {
+        const result = businessAddressSelectChangePresenter(data)
+
+        expect(result.businessName).toEqual(null)
+      })
+    })
+  })
+
+  describe('the "sbi" property', () => {
+    describe('when the sbi (singleBusinessIdentifier) property is missing', () => {
+      beforeEach(() => {
+        delete data.info.sbi
+      })
+
+      test('it should return sbi as null', () => {
+        const result = businessAddressSelectChangePresenter(data)
+
+        expect(result.sbi).toEqual(null)
+      })
+    })
+  })
+
+  describe('the "userName" property', () => {
+    describe('when the userName property is missing', () => {
+      beforeEach(() => {
+        delete data.customer.fullName
+      })
+
+      test('it should return userName as null', () => {
+        const result = businessAddressSelectChangePresenter(data)
+
+        expect(result.userName).toEqual(null)
+      })
+    })
+  })
+
+  describe('the "displayAddresses" property', () => {
+    describe('when provided with an additional address', () => {
+      beforeEach(() => {
+        data.changeBusinessAddresses.push({
+          uprn: '100000333333',
+          displayAddress: 'Willow Barn, Green Lane, Oakham Road, Little End, Middlethorpe, York, YO23 3ZX'
+        })
+      })
+
+      test('it should return the correct number of addresses for the display summary option', () => {
+        const result = businessAddressSelectChangePresenter(data)
+
+        expect(result.displayAddresses[0].text).toEqual('3 addresses found')
+      })
+    })
+  })
+})

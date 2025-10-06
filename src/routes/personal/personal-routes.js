0035c173d722d3c85a40d990d807e60068761db4
Rebecca Ransome <beckyrose200@aol.com>
2025-10-06 15:47:06 +0100

    Update: Personal routes to add address routes

--- src/routes/personal/personal-routes.js
+++ src/routes/personal/personal-routes.js
@@ -1,5 +1,13 @@
 import { personalDetailsRoutes } from './personal-details-routes.js'
+import { personalAddressEnterRoutes } from './personal-address-enter-routes.js'
+import { personalAddressCheckRoutes } from './personal-address-check-routes.js'
+import { personalAddressChangeRoutes } from './personal-address-change-routes.js'
+import { personalAddressSelectChangeRoutes } from './personal-address-select-change-routes.js'
 
 export const personalRoutes = [
-  ...personalDetailsRoutes
+  ...personalDetailsRoutes,
+  ...personalAddressEnterRoutes,
+  ...personalAddressCheckRoutes,
+  ...personalAddressChangeRoutes,
+  ...personalAddressSelectChangeRoutes
 ]

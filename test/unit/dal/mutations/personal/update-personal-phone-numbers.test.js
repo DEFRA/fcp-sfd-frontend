b0bc6b0e2553bc674c4d3813eb6d1f9397a7ca4f
Adam Kay <90856315+thousand-leaves@users.noreply.github.com>
2025-10-06 15:26:27 +0100

    SFD2-156 Personal Phone Numbers Update (#150)
    * Update: presenter and view
    
    * Add: route, schema, presenter, view and tests
    
    * Fix: inline styling console error in personal-details view
    
    * Add: presenter, view, route and tests for check page
    
    * Add: customer phone update mutation and test
    
    * Add: fetch personal change service and refactor
    
    * Fix: failing tests
    
    * Add: test coverage
    
    * Remove: comments
    
    * Add: test coverage
    
    * Refactor: conditional logic into presenter
    
    * Fix: failing tests
    
    * Revert: mock crn test data
    
    * Update: fetch-personal-change-service to handle arrays
    
    * Update: mutation file structure to use personal directory
    
    * Add: fullNameJoined property to personal details mapping and presenters
    
    * Refactor: presenter to use base presenter
    
    * Fix: tests
    
    * Fix: incorrect cache clearance

---
+++ test/unit/dal/mutations/personal/update-personal-phone-numbers.test.js
@@ -0,0 +1,23 @@
+import { parse } from 'graphql'
+import { describe, test, expect } from 'vitest'
+import { updatePersonalPhoneNumbersMutation } from '../../../../../src/dal/mutations/personal/update-personal-phone-numbers.js'
+
+describe('When the updatePersonalPhoneNumbersMutation is parsed', () => {
+  test('it is valid GraphQL syntax', () => {
+    expect(() => parse(updatePersonalPhoneNumbersMutation)).not.toThrow()
+  })
+
+  test('it contains the Mutation operation and the correct variable', () => {
+    // Parsing the GQL mutation returns a Abstract Syntax Tree (ast)
+    // this converts the string into a structural representation of the mutation.
+    // This allows it be inspected and validated.
+
+    const ast = parse(updatePersonalPhoneNumbersMutation)
+    const operation = ast.definitions[0]
+    expect(operation.name.value).toBe('Mutation')
+
+    const variable = operation.variableDefinitions[0]
+    expect(variable.variable.name.value).toBe('input')
+    expect(variable.type.type.name.value).toBe('UpdateCustomerPhoneInput')
+  })
+})
---
+++ test/unit/presenters/personal/personal-phone-numbers-change-presenter.test.js
@@ -0,0 +1,106 @@
+// Test framework dependencies
+import { describe, test, expect, beforeEach } from 'vitest'
+
+// Thing under test
+import { personalPhoneNumbersChangePresenter } from '../../../../src/presenters/personal/personal-phone-numbers-change-presenter.js'
+
+describe('personalPhoneNumbersChangePresenter', () => {
+  let data
+  let payload
+
+  beforeEach(() => {
+    data = {
+      info: {
+        fullName: {
+          fullNameJoined: 'Alfred Waldron'
+        }
+      },
+      contact: {
+        telephone: '01234 567891',
+        mobile: null
+      },
+      changePersonalPhoneNumbers: {}
+    }
+  })
+
+  describe('when provided with personal phone numbers change data', () => {
+    test('it correctly presents the data', () => {
+      const result = personalPhoneNumbersChangePresenter(data)
+
+      expect(result).toEqual({
+        backLink: { href: '/personal-details' },
+        pageTitle: 'What are your personal phone numbers?',
+        metaDescription: 'Update the phone numbers for your personal account.',
+        userName: 'Alfred Waldron',
+        personalTelephone: '01234 567891',
+        personalMobile: null
+      })
+    })
+  })
+
+  describe('the "userName" property', () => {
+    describe('when the userName property is missing', () => {
+      beforeEach(() => {
+        delete data.info.fullName.fullNameJoined
+      })
+
+      test('it should return userName as null', () => {
+        const result = personalPhoneNumbersChangePresenter(data)
+
+        expect(result.userName).toEqual(null)
+      })
+    })
+  })
+
+  describe('the "personalMobile" property', () => {
+    describe('when provided with a changed personalMobile', () => {
+      beforeEach(() => {
+        data.changePersonalPhoneNumbers.personalMobile = '01111 111111'
+      })
+
+      test('it should return personalMobile as the changed personalMobile', () => {
+        const result = personalPhoneNumbersChangePresenter(data)
+
+        expect(result.personalMobile).toEqual('01111 111111')
+      })
+    })
+
+    describe('when provided with a payload', () => {
+      beforeEach(() => {
+        payload = { personalMobile: '01234 111111' }
+      })
+
+      test('it should return personalMobile as the payload', () => {
+        const result = personalPhoneNumbersChangePresenter(data, payload)
+
+        expect(result.personalMobile).toEqual('01234 111111')
+      })
+    })
+  })
+
+  describe('the "personalTelephone" property', () => {
+    describe('when provided with a changed personalTelephone', () => {
+      beforeEach(() => {
+        data.changePersonalPhoneNumbers.personalTelephone = '01214 151151'
+      })
+
+      test('it should return personalTelephone as the changed personalTelephone', () => {
+        const result = personalPhoneNumbersChangePresenter(data)
+
+        expect(result.personalTelephone).toEqual('01214 151151')
+      })
+    })
+
+    describe('when provided with a payload', () => {
+      beforeEach(() => {
+        payload = { personalTelephone: '02222 222222' }
+      })
+
+      test('it should return personalTelephone as the payload', () => {
+        const result = personalPhoneNumbersChangePresenter(data, payload)
+
+        expect(result.personalTelephone).toEqual('02222 222222')
+      })
+    })
+  })
+})

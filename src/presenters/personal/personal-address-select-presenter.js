baa8ca75c22ed9b1e85504fad96d6a07c1a0f046
Rebecca Ransome <beckyrose200@aol.com>
2025-10-08 16:27:15 +0100

    Add: Personal address select presenter and test

---
+++ src/presenters/personal/personal-address-select-presenter.js
@@ -0,0 +1,58 @@
+/**
+ * Formats data ready for presenting in the `/account-address-select` page
+ * @module personalAddressSelectPresenter
+ */
+
+const personalAddressSelectPresenter = (data) => {
+  return {
+    backLink: { href: '/account-address-change' },
+    pageTitle: 'Choose your personal address',
+    metaDescription: 'Choose the address for your personal account.',
+    userName: data.info.fullName.fullNameJoined ?? null,
+    postcode: data.changePersonalPostcode.postcode,
+    displayAddresses: formatDisplayAddresses(data.changePersonalAddresses)
+  }
+}
+
+/**
+ * Formats a list of address objects for display in a dropdown menu.
+ *
+ * Each address is transformed into an option with:
+ * - `value`: a concatenation of the address UPRN and displayAddress
+ * - `text`: the displayAddress property from the address object. This is a fully
+ *   formatted address string, e.g. "123 Main Street, Manchester, M1 1AA"
+ * - `selected`: false for all addresses
+ *
+ * A summary option is also prepended to the start of the list,
+ * showing how many addresses were found. This summary option is
+ * marked as selected by default.
+ *
+ * Note: the `value` is constructed by concatenating `uprn` and `displayAddress`
+ * because during testing some addresses were found to share the same UPRN (although rare).
+ * Postcode LL55 2NF in particular had a few.
+ * Using the UPRN alone as the identifying value caused the wrong address to be returned.
+ * Concatenating the UPRN with the full display address ensures the dropdown option value is unique for each entry.
+ */
+function formatDisplayAddresses (addresses) {
+  const displayAddresses = addresses.map(address => ({
+    value: `${address.uprn}${address.displayAddress}`,
+    text: address.displayAddress,
+    selected: false
+  }))
+
+  // Add a display summary option to the beginning of the list
+  // e.g. "18 addresses found"
+  const text = addresses.length === 1 ? '1 address found' : `${addresses.length} addresses found`
+
+  displayAddresses.unshift({
+    value: 'display',
+    text,
+    selected: true
+  })
+
+  return displayAddresses
+}
+
+export {
+  personalAddressSelectPresenter
+}

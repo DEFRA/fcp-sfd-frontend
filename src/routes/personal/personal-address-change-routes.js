d68a548ecdb8b5c2a5c36215c86024022ca8beb2
Adam Kay <thousand_leaves@hotmail.com>
2025-10-10 16:19:33 +0100

    Fix: create constant to please Sonar

--- src/routes/personal/personal-address-change-routes.js
+++ src/routes/personal/personal-address-change-routes.js
@@ -6,6 +6,8 @@ import { setSessionData } from '../../utils/session/set-session-data.js'
 import { addressLookupService } from '../../services/os-places/address-lookup-service.js'
 import { personalAddressChangeErrorService } from '../../services/personal/personal-address-change-error-service.js'
 
+const PERSONAL_ADDRESS_CHANGE_VIEW = 'personal/personal-address-change'
+
 const getPersonalAddressChange = {
   method: 'GET',
   path: '/account-address-change',
@@ -14,7 +16,7 @@ const getPersonalAddressChange = {
     const personalDetails = await fetchPersonalChangeService(yar, auth.credentials, 'changePersonalPostcode')
     const pageData = personalAddressChangePresenter(personalDetails)
 
-    return h.view('personal/personal-address-change', pageData)
+    return h.view(PERSONAL_ADDRESS_CHANGE_VIEW, pageData)
   }
 }
 
@@ -29,7 +31,7 @@ const postPersonalAddressChange = {
         const { yar, auth, payload } = request
         const pageData = await personalAddressChangeErrorService(yar, auth.credentials, payload.personalPostcode, err.details)
 
-        return h.view('personal/personal-address-change', pageData).code(BAD_REQUEST).takeover()
+        return h.view(PERSONAL_ADDRESS_CHANGE_VIEW, pageData).code(BAD_REQUEST).takeover()
       }
     }
   },
@@ -42,7 +44,7 @@ const postPersonalAddressChange = {
     if (addresses.error) {
       const pageData = await personalAddressChangeErrorService(yar, auth.credentials, payload.personalPostcode, addresses.error)
 
-      return h.view('personal/personal-address-change', pageData).code(BAD_REQUEST).takeover()
+      return h.view(PERSONAL_ADDRESS_CHANGE_VIEW, pageData).code(BAD_REQUEST).takeover()
     }
 
     return h.redirect('/account-address-select')

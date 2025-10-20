b339b3ed792f8d42eb891749743e8cbca968bf8d
Adam Kay <thousand_leaves@hotmail.com>
2025-10-20 12:16:00 +0100

    Add: routes

--- src/routes/personal/personal-routes.js
+++ src/routes/personal/personal-routes.js
@@ -1,9 +1,13 @@
 import { personalDetailsRoutes } from './personal-details-routes.js'
+import { personalNameChangeRoutes } from './personal-name-change-routes.js'
+import { personalNameCheckRoutes } from './personal-name-check-routes.js'
 import { personalPhoneNumbersChangeRoutes } from './personal-phone-numbers-change-routes.js'
 import { personalPhoneNumbersCheckRoutes } from './personal-phone-numbers-check-routes.js'
 
 export const personalRoutes = [
   ...personalDetailsRoutes,
+  ...personalNameChangeRoutes,
+  ...personalNameCheckRoutes,
   ...personalPhoneNumbersChangeRoutes,
   ...personalPhoneNumbersCheckRoutes
 ]

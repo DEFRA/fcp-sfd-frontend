d6bd6cb5409f3c22abcd29181d147c8b04c82490
Rebecca Ransome <beckyrose200@aol.com>
2025-10-23 14:19:40 +0100

    Fix: date not displaying in change page correctly

--- src/presenters/personal/personal-dob-change-presenter.js
+++ src/presenters/personal/personal-dob-change-presenter.js
@@ -4,14 +4,17 @@
  */
 
 const personalDobChangePresenter = (data, payload) => {
-  const dateInputValues = generateDateInputValues(data, payload)
+  const { day, month, year} = generateDateInputValues(data, payload)
+
   return {
     backLink: { href: '/personal-details' },
     userName: data.info.fullName.fullNameJoined ?? null,
     pageTitle: 'What is your date of birth?',
     metaDescription: 'Update the date of birth for your personal account.',
     hint: 'For example, 31 3 1980',
-    ...dateInputValues
+    day,
+    month,
+    year
   }
 }
 
@@ -23,11 +26,13 @@ const generateDateInputValues = (data, payload) => {
       year: payload.year || ''
     }
   }
-  const personalDob = new Date([data.info.dateOfBirth])
+
+  const [year, month, day] = data.info.dateOfBirth.split('-').map(Number)
+
   return {
-    day: data.changePersonalDob?.day || personalDob.getDate(),
-    month: data.changePersonalDob?.month || personalDob.getMonth() + 1,
-    year: data.changePersonalDob?.year || personalDob.getFullYear()
+    day: data.changePersonalDob?.day || `${day}`,
+    month: data.changePersonalDob?.month || `${month}`,
+    year: data.changePersonalDob?.year || `${year}`
   }
 }
 

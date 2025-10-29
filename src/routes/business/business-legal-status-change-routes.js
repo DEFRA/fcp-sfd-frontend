104aceb126823ac8684a2237a5db0a53e1024209
Rebecca Ransome <beckyrose200@aol.com>
2025-10-29 19:44:20 +0000

    Add: FULL_PERMISSIONS to business legal status change route

--- src/routes/business/business-legal-status-change-routes.js
+++ src/routes/business/business-legal-status-change-routes.js
@@ -1,9 +1,13 @@
 import { fetchBusinessDetailsService } from '../../services/business/fetch-business-details-service.js'
 import { businessLegalStatusChangePresenter } from '../../presenters/business/business-legal-status-change-presenter.js'
+import { FULL_PERMISSIONS } from '../../constants/scope/business-details.js'
 
 const getBusinessLegalStatusChange = {
   method: 'GET',
   path: '/business-legal-status-change',
+  options: {
+    auth: { scope: FULL_PERMISSIONS }
+  },
   handler: async (request, h) => {
     const businessDetails = await fetchBusinessDetailsService(request.auth.credentials)
     const pageData = businessLegalStatusChangePresenter(businessDetails)

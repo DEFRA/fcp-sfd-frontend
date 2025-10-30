6b67b3565755a47d18ff0c8aab97d2cec1d80762
Rebecca Ransome <beckyrose200@aol.com>
2025-10-30 15:03:41 +0000

    Add: Home presenter and test

---
+++ src/presenters/home-presenter.js
@@ -0,0 +1,31 @@
+/**
+ * Formats data ready for presenting in the `/home` page
+ * @module homePresenter
+ */
+
+import { VIEW_LEVEL_PERMISSION } from '../constants/scope/business-details.js'
+
+const homePresenter = (authData) => {
+  return {
+    userName: authData.name ?? null,
+    businessDetails: setBusinessDetails(authData.credentials.scope),
+  }
+}
+
+const setBusinessDetails = (permissionGroups) => {
+  if (permissionGroups.includes(VIEW_LEVEL_PERMISSION)) {
+    return {
+      link: '/business-details',
+      text: 'View business details'
+    }
+  }
+
+  return {
+    link: '/business-details',
+    text: 'View and update your business details'
+  }
+}
+
+export {
+  homePresenter
+}

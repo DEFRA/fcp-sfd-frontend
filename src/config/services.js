59d4e185dde55a8d0771ea322b5c28478dd800b0
Fay Toward <fay.toward@defra.gov.uk>
2025-11-04 16:53:06 +0000

    Fix indentation in services configuration for consistency

--- src/config/services.js
+++ src/config/services.js
@@ -1,10 +1,10 @@
 export const servicesConfig = {
-    servicesConfig: {
-        IAHWEndpoint: {
-            doc: 'Endpoint to reach IAHW',
-            format: String,
-            default: null,
-            env: 'IAHW_ENDPOINT'
-        }
+  servicesConfig: {
+    IAHWEndpoint: {
+      doc: 'Endpoint to reach IAHW',
+      format: String,
+      default: null,
+      env: 'IAHW_ENDPOINT'
     }
+  }
 }

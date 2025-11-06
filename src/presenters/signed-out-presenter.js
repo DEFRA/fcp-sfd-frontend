171bebac892f4c57cf4804a65c7fdfbafaa3dae8
Adam Kay <thousand_leaves@hotmail.com>
2025-11-06 14:56:22 +0000

    Update: text on the sign-out page

--- src/presenters/signed-out-presenter.js
+++ src/presenters/signed-out-presenter.js
@@ -7,7 +7,7 @@ const signedOutPresenter = () => {
   return {
     pageTitle: 'You have signed out',
     metaDescription: 'If this is not what you wanted, you can',
-    signInText: 'sign back into Land and farm service',
+    signInText: 'sign back into the Land and farm service',
     signInLink: '/auth/sign-in'
   }
 }

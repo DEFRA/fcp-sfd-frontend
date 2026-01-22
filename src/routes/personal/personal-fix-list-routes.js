acf560e4435c6fcb40bc049998c248b754153e8f
Rebecca Ransome <beckyrose200@aol.com>
2026-01-22 14:47:54 +0000

    Add: Personal fix list routes and test

---
+++ src/routes/personal/personal-fix-list-routes.js
@@ -0,0 +1,47 @@
+import { setPersonalFixSessionDataService } from '../../services/personal/set-personal-fix-session-data-service.js'
+import { personalFixListPresenter } from '../../presenters/personal/personal-fix-list-presenter.js'
+import { validatePersonalFixService } from '../../services/personal/validate-personal-fix-service.js'
+import { formatValidationErrors } from '../../utils/format-validation-errors.js'
+import { fetchPersonalFixService } from '../../services/personal/fetch-personal-fix-service.js'
+
+const getPersonalFixList = {
+  method: 'GET',
+  path: '/personal-fix-list',
+  handler: async (request, h) => {
+    const { yar, auth } = request
+
+    const sessionData = yar.get('personalDetailsValidation') || {}
+    const personalDetails = await fetchPersonalFixService(auth.credentials, sessionData)
+    const pageData = personalFixListPresenter(personalDetails)
+
+    return h.view('personal/personal-fix-list.njk', pageData)
+  }
+}
+
+const postPersonalFixList = {
+  method: 'POST',
+  path: '/personal-fix-list',
+  handler: async (request, h) => {
+    const { yar, auth, payload } = request
+
+    const sessionData = yar.get('personalDetailsValidation')
+    const validation = validatePersonalFixService(payload, sessionData.orderedSectionsToFix)
+
+    if (validation.error) {
+      const errors = formatValidationErrors(validation.error.details || [])
+      const personalDetails = await fetchPersonalFixService(auth.credentials, sessionData)
+      const pageData = personalFixListPresenter(personalDetails, payload, errors)
+
+      return h.view('personal/personal-fix-list.njk', { ...pageData, errors }).code(400)
+    }
+
+    setPersonalFixSessionDataService(yar, sessionData, payload)
+
+    return h.redirect('/personal-fix-check')
+  }
+}
+
+export const personalFixListRoutes = [
+  getPersonalFixList,
+  postPersonalFixList
+]

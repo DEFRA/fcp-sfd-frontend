0592d212d2c7eb60bcbab06334a704e2277b3481
Adam Kay <thousand_leaves@hotmail.com>
2026-01-22 15:40:30 +0000

    Update: business details presenter and view

--- src/presenters/business/business-details-presenter.js
+++ src/presenters/business/business-details-presenter.js
@@ -4,11 +4,15 @@
  */
 
 import { formatBackLink, formatDisplayAddress } from '../base-presenter.js'
+import { config } from '../../config/index.js'
 import { FULL_PERMISSIONS, AMEND_PERMISSIONS } from '../../constants/scope/business-details.js'
 
 const businessDetailsPresenter = (data, yar, permissionLevels) => {
   const permissionGroup = checkPermissionType(permissionLevels)
-  const countyParishHoldingNumbers = formatCph(data.info.countyParishHoldingNumbers)
+  const cphEnabled = config.get('featureToggle.cphEnabled')
+  const countyParishHoldingNumbers = cphEnabled
+    ? formatCph(data.info.countyParishHoldingNumbers)
+    : []
 
   return {
     backLink: {
@@ -32,6 +36,7 @@ const businessDetailsPresenter = (data, yar, permissionLevels) => {
     vendorRegistrationNumber: data.info.vendorNumber ?? null,
     countyParishHoldingNumbers,
     countyParishHoldingNumbersText: `County Parish Holding (CPH) number${countyParishHoldingNumbers.length > 1 ? 's' : ''}`,
+    showCph: cphEnabled,
     businessLegalStatus: data.info.legalStatus,
     businessType: data.info.type,
     changeLinks: setChangeLinks(data, permissionGroup),

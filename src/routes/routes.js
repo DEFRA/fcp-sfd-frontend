7894b5d987a557432a07c82d1a2eca86b4006899
Adam Kay <thousand_leaves@hotmail.com>
2026-02-19 16:07:58 +0000

    Refactor: reorganize footer routes and tests

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,21 +1,19 @@
-import { errors } from './errors/error-routes.js'
 import { health } from './health-routes.js'
 import { auth } from './auth-routes.js'
 import { homeRoutes } from './home-routes.js'
-import { staticAssetRoutes } from './static-assets-routes.js'
-import { cookies } from './cookies-routes.js'
+import { errors } from './errors/error-routes.js'
 import { signedOut } from './signed-out-routes.js'
+import { staticAssetRoutes } from './static-assets-routes.js'
 import { footerRoutes } from './footer/footer-routes.js'
 import { businessRoutes } from './business/business-routes.js'
 import { personalRoutes } from './personal/personal-routes.js'
 
 export const routes = [
   health,
-  ...homeRoutes,
   ...auth,
-  cookies,
-  signedOut,
+  ...homeRoutes,
   ...errors,
+  signedOut,
   ...staticAssetRoutes,
   ...footerRoutes,
   ...businessRoutes,

dd098732d88c33545e6fbc7f465c73806cfd88e6
Rana Salem <ranatasalem@gmail.com>
2026-02-19 15:14:03 +0000

    update personal-fix page to correctly pull in userName for the full name banner

--- src/routes/personal/personal-fix-routes.js
+++ src/routes/personal/personal-fix-routes.js
@@ -1,14 +1,16 @@
 import { personalFixPresenter } from '../../presenters/personal/personal-fix-presenter.js'
 import { initialiseFixJourneyService } from '../../services/initialise-fix-journey-service.js'
+import { fetchPersonalFixService } from '../../services/personal/fetch-personal-fix-service.js'
 
 const getPersonalFix = {
   method: 'GET',
   path: '/personal-fix',
   handler: async (request, h) => {
-    const { yar, query } = request
+    const { yar, query, auth } = request
 
     const sessionData = initialiseFixJourneyService(yar, query.source, 'personal')
-    const pageData = personalFixPresenter(sessionData)
+    const personalDetails = await fetchPersonalFixService(auth.credentials, sessionData)
+    const pageData = personalFixPresenter(personalDetails)
 
     return h.view('personal/personal-fix.njk', pageData)
   }

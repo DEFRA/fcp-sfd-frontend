6b1ff91b28c5241882ebb905c2a671310d3f25e0
Adam Kay <thousand_leaves@hotmail.com>
2026-02-20 15:45:49 +0000

    Fix: whitespace and formatting issues

--- test/unit/mappers/permissions-mapper.test.js
+++ test/unit/mappers/permissions-mapper.test.js
@@ -13,27 +13,27 @@ const { mapPermissions } = await import('../../../src/mappers/permissions-mapper
 const mockLogger = createLogger()
 
 describe('permissionsMapper', () => {
-  describe('when given valid raw DAL data ', () => {
-    test('it should map the values to the correct format ', () => {
+  describe('when given valid raw DAL data', () => {
+    test('it should map the values to the correct format', () => {
       const result = mapPermissions(dalData)
       expect(result).toEqual(mappedData)
     })
 
-    test('it should build the privileges correctly ', () => {
+    test('it should build the privileges correctly', () => {
       const result = mapPermissions(dalData)
 
       expect(result.privileges).toEqual(mappedData.privileges)
     })
 
-    test('it should build the businessName correctly ', () => {
+    test('it should build the businessName correctly', () => {
       const result = mapPermissions(dalData)
 
       expect(result.businessName).toEqual(mappedData.businessName)
     })
   })
 
-  describe('when given invalid raw DAL data ', () => {
-    test('it should log a warning about invalid data ', () => {
+  describe('when given invalid raw DAL data', () => {
+    test('it should log a warning about invalid data', () => {
       const invalidData = {}
 
       const wrapper = () => mapPermissions(invalidData)

01d819a850363a5321accc5e7bf08b382cb6bd99
Rebecca Ransome <beckyrose200@aol.com>
2026-02-20 12:30:26 +0000

    Update: Status codes

--- src/routes/catch-all-routes.js
+++ src/routes/catch-all-routes.js
@@ -1,3 +1,5 @@
+import { NOT_FOUND } from '../constants/status-codes.js'
+
 export const catchAllNotFound = {
   method: '*',
   path: '/{any*}', // matches any path that hasn't been matched by previous routes
@@ -5,6 +7,6 @@ export const catchAllNotFound = {
     auth: { strategy: 'session', mode: 'try' }
   },
   handler: (_request, h) => {
-    return h.view('errors/page-not-found').code(404)
+    return h.view('errors/page-not-found').code(NOT_FOUND)
   }
 }

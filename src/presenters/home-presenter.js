8698b32fadea4364387a3778946869d030f7cc2b
Rebecca Ransome <beckyrose200@aol.com>
2026-03-25 17:46:31 +0000

    Update: Home presenter to use fptt card

--- src/presenters/home-presenter.js
+++ src/presenters/home-presenter.js
@@ -5,7 +5,7 @@
 
 import { VIEW_LEVEL_PERMISSION } from '../constants/scope/business-details.js'
 
-const homePresenter = (data, permissionGroups, enrolmentCount) => {
+const homePresenter = (data, permissionGroups, enrolmentCount, isOnFarmingPaymentsWhitelist) => {
   const presentedData = {
     pageTitle: 'Your business',
     metaDescription: 'Home page for your business\'s schemes and details.',
@@ -13,11 +13,12 @@ const homePresenter = (data, permissionGroups, enrolmentCount) => {
     businessName: data.business.info.name,
     businessDetails: setBusinessDetails(permissionGroups),
     sbi: data.business.info.sbi,
-    isOnFarmingPaymentsWhitelist: true,
+    isOnFarmingPaymentsWhitelist,
     farmingPayments: {
       link: 'fp-check-your-details',
       title: 'Farm Payments Technical Test',
-      status: 'started'
+      // Status hidden for now; endpoint from FPTT not available yet. Will update in a future ticket.
+      status: 'do-not-show'
     }
   }
 

9c9d9ec16a6676e558ca7b5930409c445093e46f
Adam Kay <thousand_leaves@hotmail.com>
2026-03-31 12:02:52 +0100

    Remove: redundant getTokenCache implementation and update tests

--- src/utils/caching/token-cache.js
+++ src/utils/caching/token-cache.js
@@ -1,19 +1,9 @@
 import { config } from '../../config/index.js'
 
-let tokenCache = null
-
 export const initTokenCache = (server, cacheName) => {
-  tokenCache = server.cache({
+  return server.cache({
     cache: cacheName,
     segment: config.get('server.session.cache.tokenSegment'),
     expiresIn: config.get('redis.ttl')
   })
-  return tokenCache
-}
-
-export const getTokenCache = () => {
-  if (!tokenCache) {
-    throw new Error('Token cache is not initialized.')
-  }
-  return tokenCache
 }

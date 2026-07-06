ed4fab8a62dc2a0508b277c823a6d5514d27f904
Adam Kay <thousand_leaves@hotmail.com>
2026-07-06 16:34:15 +0100

    Fix missing pafOrganisationName in business details query

--- src/dal/queries/business-details.js
+++ src/dal/queries/business-details.js
@@ -77,6 +77,7 @@ export const businessDetailsQueryWithoutCph = `
         type
       }
       address {
+        pafOrganisationName
         buildingNumberRange
         buildingName
         flatName

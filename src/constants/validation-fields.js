45e9351bd727b0374288e5a7f4ad35438f7e4bb9
Rebecca Ransome <beckyrose200@aol.com>
2026-07-06 17:07:36 +0100

    Remove: Unused constant

--- src/constants/validation-fields.js
+++ src/constants/validation-fields.js
@@ -2,7 +2,6 @@ export const BUSINESS_NAME_MAX = 160
 export const EMAIL_MAX = 254
 export const PHONE_NUMBER_MIN = 10
 export const PHONE_NUMBER_MAX = 50
-export const MAX_AGE_YEARS = 120
 
 // Address
 export const POSTCODE_MAX = 8

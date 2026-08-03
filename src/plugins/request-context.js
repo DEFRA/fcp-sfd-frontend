6622c89ea1f1026d9c7ded9088be76ba9b76f539
Gabor Koos <gabor.koos@gmail.com>
2026-08-03 18:00:53 +0100

    feat: add request context plugin to log user context in event fields

---
+++ src/plugins/request-context.js
@@ -0,0 +1,20 @@
+import { maskCrn } from '../utils/mask-crn.js'
+
+export const requestContext = {
+  plugin: {
+    name: 'request-context',
+    register: (server) => {
+      server.ext('onPreResponse', (request, h) => {
+        const credentials = request.auth?.credentials
+        request.logger = request.logger.child({
+          event: {
+            reference: credentials?.crn ? `crn-${maskCrn(credentials.crn)}` : '',
+            category: credentials?.sbi ? `sbi-${credentials.sbi}` : '',
+            type: credentials?.sessionId ? `session_id-${credentials.sessionId}` : ''
+          }
+        })
+        return h.continue
+      })
+    }
+  }
+}

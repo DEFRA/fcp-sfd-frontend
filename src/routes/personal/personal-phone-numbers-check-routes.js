fe605c254310e6ac866ed82863115705367f3838
Rebecca Ransome <beckyrose200@aol.com>
2026-08-11 21:31:08 +0100

    Update: Personal phone numbers check route and test

--- src/routes/personal/personal-phone-numbers-check-routes.js
+++ src/routes/personal/personal-phone-numbers-check-routes.js
@@ -1,13 +1,18 @@
 import { personalPhoneNumbersCheckPresenter } from '../../presenters/personal/personal-phone-numbers-check-presenter.js'
 import { fetchPersonalChangeService } from '../../services/personal/fetch-personal-change-service.js'
 import { updatePersonalPhoneNumbersChangeService } from '../../services/personal/update-personal-phone-numbers-change-service.js'
+import { checkSessionDataGuard } from '../pre-handlers.js'
 
 const getPersonalPhoneNumbersCheck = {
   method: 'GET',
   path: '/account-phone-numbers-check',
+  options: {
+    pre: [checkSessionDataGuard('changePersonalPhoneNumbers', '/personal-details', 'personalDetailsUpdate')]
+  },
   handler: async (request, h) => {
     const { yar, auth } = request
     const personalDetails = await fetchPersonalChangeService(yar, auth.credentials, 'changePersonalPhoneNumbers')
+
     const pageData = personalPhoneNumbersCheckPresenter(personalDetails)
 
     return h.view('personal/personal-phone-numbers-check', pageData)

a3318181db5ea1611fdc14b0cdf2bd36258128fe
Rebecca Ransome <beckyrose200@aol.com>
2026-08-11 21:30:42 +0100

    Update: Personal name check route and test

--- src/routes/personal/personal-name-check-routes.js
+++ src/routes/personal/personal-name-check-routes.js
@@ -1,13 +1,18 @@
 import { personalNameCheckPresenter } from '../../presenters/personal/personal-name-check-presenter.js'
 import { fetchPersonalChangeService } from '../../services/personal/fetch-personal-change-service.js'
 import { updatePersonalNameChangeService } from '../../services/personal/update-personal-name-change-service.js'
+import { checkSessionDataGuard } from '../pre-handlers.js'
 
 const getPersonalNameCheck = {
   method: 'GET',
   path: '/account-name-check',
+  options: {
+    pre: [checkSessionDataGuard('changePersonalName', '/personal-details', 'personalDetailsUpdate')]
+  },
   handler: async (request, h) => {
     const { yar, auth } = request
     const personalDetails = await fetchPersonalChangeService(yar, auth.credentials, 'changePersonalName')
+
     const pageData = personalNameCheckPresenter(personalDetails)
 
     return h.view('personal/personal-name-check', pageData)

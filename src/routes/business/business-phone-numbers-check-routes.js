f6c3c4e672a96c530ed39ce21e9300fb036293f6
Rebecca Ransome <beckyrose200@aol.com>
2026-08-11 21:30:00 +0100

    Update: Business phone numbers check route and test

--- src/routes/business/business-phone-numbers-check-routes.js
+++ src/routes/business/business-phone-numbers-check-routes.js
@@ -2,16 +2,19 @@ import { businessPhoneNumbersCheckPresenter } from '../../presenters/business/bu
 import { fetchBusinessChangeService } from '../../services/business/fetch-business-change-service.js'
 import { updateBusinessPhoneNumbersChangeService } from '../../services/business/update-business-phone-numbers-change-service.js'
 import { AMEND_PERMISSIONS } from '../../constants/scope/business-details.js'
+import { checkSessionDataGuard } from '../pre-handlers.js'
 
 const getBusinessPhoneNumbersCheck = {
   method: 'GET',
   path: '/business-phone-numbers-check',
   options: {
-    auth: { scope: AMEND_PERMISSIONS }
+    auth: { scope: AMEND_PERMISSIONS },
+    pre: [checkSessionDataGuard('changeBusinessPhoneNumbers', '/business-details', 'businessDetailsUpdate')]
   },
   handler: async (request, h) => {
     const { yar, auth } = request
     const businessDetails = await fetchBusinessChangeService(yar, auth.credentials, 'changeBusinessPhoneNumbers')
+
     const pageData = businessPhoneNumbersCheckPresenter(businessDetails)
 
     return h.view('business/business-phone-numbers-check', pageData)

c7b2ff05068179c071dd23d073cafbe1872ac5af
Rana Salem <127218837+rtasalem@users.noreply.github.com>
2026-08-26 11:31:35 +0100

    FLS2-275 Bump frontend engine to use new VAT schema (#500)

--- test/mocks/mock-business-details.js
+++ test/mocks/mock-business-details.js
@@ -58,6 +58,11 @@ const getMappedData = () => ({
     traderNumber: '010203040506070880980',
     vendorNumber: '694523',
     legalStatus: 'Sole Proprietorship',
+    legalStatusCode: 102111,
+    registrationNumbers: {
+      companiesHouse: null,
+      charityCommission: null
+    },
     type: 'Not Specified',
     countyParishHoldingNumbers: [{ cphNumber: '12/123/1234' }]
   },

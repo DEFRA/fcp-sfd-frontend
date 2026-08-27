75853eec735687088c7c47055f890511476ab075
Rana Salem <ranatasalem@gmail.com>
2026-08-27 23:27:05 +0100

    update docs for google analytics config module

--- src/config/google-analytics.js
+++ src/config/google-analytics.js
@@ -3,11 +3,11 @@ export const googleAnalyticsConfig = {
     googleTagManagerKey: {
       doc: 'Google Tag Manager (GTM) key, also called the container ID for connecting the fcp-sfd-frontend service to Google Analytics',
       format: String,
-      default: '',
+      default: undefined,
       env: 'GOOGLE_TAG_MANAGER_KEY'
     },
     enabled: {
-      doc: 'Check if the GTM key is present and enable Google Analytics',
+      doc: 'Check if the GTM key is present and if so enable Google Analytics',
       format: Boolean,
       default: !!process.env.GOOGLE_TAG_MANAGER_KEY
     }

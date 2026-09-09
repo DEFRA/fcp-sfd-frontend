2a1e4cefd0b5e30b92b77bd031406fcfafb4716e
Adam Kay <thousand_leaves@hotmail.com>
2026-09-09 11:24:53 +0100

    Use Unified Business Details Update Mutation

--- src/services/business/update-business-fix-service.js
+++ src/services/business/update-business-fix-service.js
@@ -6,16 +6,13 @@
 import { fetchBusinessFixService } from './fetch-business-fix-service.js'
 import { updateDalService } from '../DAL/update-dal-service.js'
 import { flashNotification } from '../../utils/notifications/flash-notification.js'
-import { buildBusinessUpdateVariablesService } from './build-business-update-variables-service.js'
-import { buildBusinessDetailsMutationService } from './build-business-details-mutation-service.js'
-import { services } from '@defra/fcp-sfd-frontend-engine'
+import { mutations, services } from '@defra/fcp-sfd-frontend-engine'
 
 const updateBusinessFixService = async (sessionData, yar, credentials) => {
   const businessDetails = await fetchBusinessFixService(credentials, sessionData)
-  const variables = buildBusinessUpdateVariablesService(businessDetails)
-  const updateBusinessDetailsMutation = buildBusinessDetailsMutationService(businessDetails.orderedSectionsToFix)
+  const variables = services.buildBusinessFixUpdateVariables(businessDetails)
 
-  await updateDalService(updateBusinessDetailsMutation, variables, credentials.sessionId)
+  await updateDalService(mutations.updateBusinessDetails, variables, credentials.sessionId)
 
   yar.clear('businessDetails')
 
